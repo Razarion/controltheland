29dc0785ac46468cb5ea01b363f0f84437a93b50
beat.keller@btxtech.com <beat.keller@btxtech.com@4948f9da-d142-11de-aacf-132551bdafba>
2010-01-24 17:01:05 +0000

    Speech bubble improved

--- trunk/src/com/btxtech/game/jsre/client/utg/SpeechBubble.java
+++ trunk/src/com/btxtech/game/jsre/client/utg/SpeechBubble.java
@@ -16,6 +16,7 @@ package com.btxtech.game.jsre.client.utg;
 import com.btxtech.game.jsre.client.ExtendedCanvas;
 import com.btxtech.game.jsre.client.common.Constants;
 import com.btxtech.game.jsre.client.terrain.MapWindow;
+import com.btxtech.game.jsre.client.terrain.TerrainView;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.widgetideas.graphics.client.Color;
@@ -26,22 +27,59 @@ import com.google.gwt.widgetideas.graphics.client.Color;
  * Time: 21:59:32
  */
 public class SpeechBubble extends AbsolutePanel {
+    public static final int SPACING = 2;
     public static final int CURVE_SIZE = 40;
-    public static final int BEAK_HEIGHT = 30;
+    public static final int BUBLLE_BORDER_SIZE = 40;
+    public static final int BEAK_LENGTH = 30;
     public static final int BEAK_WIDTH = 30;
+    public static final int LINE_WIDTH = 2;
 
-    public SpeechBubble() {
-        setPixelSize(200, 200);
+    enum Direction {
+        TOP,
+        LEFT,
+        BOTTOM,
+        RIGHT
+    }
+
+    public SpeechBubble(int beakRelX, int beakRelY) {
+        int scrrenWidth = TerrainView.getInstance().getViewWidth();
+        int htmlWidth = 200;
+        int htmlHeight = 200;
+        int bubblWidth = htmlWidth + 2 * BUBLLE_BORDER_SIZE + 2 * LINE_WIDTH;
+        int bubblHeight = htmlHeight + 2 * BUBLLE_BORDER_SIZE + 2 * LINE_WIDTH + BEAK_LENGTH;
+
+        Direction direction = Direction.TOP;
+        int left;
+        int beakOffset;
+        if (beakRelX < SPACING + CURVE_SIZE + BEAK_WIDTH / 2) {
+            left = SPACING;
+            beakOffset = beakRelX;
+        } else if (beakRelX > scrrenWidth - bubblWidth - SPACING) {
+            left = scrrenWidth - SPACING - bubblWidth;
+            beakOffset = beakRelX - left;
+        } else {
+            beakOffset = BEAK_WIDTH + CURVE_SIZE;
+            left = beakRelX - BEAK_WIDTH - CURVE_SIZE;
+        }
+
+
+        int top = beakRelY;
+
+        ////
+        setPixelSize(bubblWidth, bubblHeight);
         getElement().getStyle().setBackgroundColor("#888888");
         getElement().getStyle().setZIndex(Constants.Z_INDEX_SPEECH_BUBBLE);
-        MapWindow.getAbsolutePanel().add(this, 200, 100);
+        MapWindow.getAbsolutePanel().add(this, left, top);
         // HTML content
         HTML content = new HTML("<h3>Hallo Du</h3> sdffsdaaf sdafsdaf sdfsdafasdf sdafsdafsdaf");
         content.getElement().getStyle().setZIndex(2);
-        add(content, 10, 10);
+        content.setPixelSize(htmlWidth, htmlHeight);
+        add(content, BUBLLE_BORDER_SIZE, BUBLLE_BORDER_SIZE + BEAK_LENGTH);
+        ////
+        buildBubble(LINE_WIDTH, LINE_WIDTH, bubblWidth - LINE_WIDTH, bubblHeight - LINE_WIDTH, beakOffset, direction);
 
         //buildBubble(2, 2, 198, 198, 199);
-        buildBubble(2, 2, 198, 198, 144);
+        //buildBubble(2, 2, 198, 198, 144);
 
         //buildBubble(2, 2, 198, 198, 1);
         //buildBubble(2, 2, 198, 198, 56);
@@ -51,31 +89,44 @@ public class SpeechBubble extends AbsolutePanel {
         //buildBubble(2, 2, 198, 198, 100);
     }
 
-    private void buildBubble(int left, int top, int right, int bottom, int beakOffset) {
-        int bodyLeft = left/* + BEAK_HEIGHT*/;
-        int bodyTop = top/* + BEAK_HEIGHT*/;
-        int bodyRight = right - BEAK_HEIGHT;
-        int bodyBottom = bottom/* - BEAK_HEIGHT*/;
+    private void buildBubble(int left, int top, int right, int bottom, int beakOffset, Direction direction) {
+        int bodyLeft = left;
+        int bodyTop = top;
+        int bodyRight = right;
+        int bodyBottom = bottom;
+        switch (direction) {
+            case BOTTOM:
+                bodyBottom -= BEAK_LENGTH;
+                break;
+            case LEFT:
+                bodyLeft += BEAK_LENGTH;
+                break;
+            case RIGHT:
+                bodyRight -= BEAK_LENGTH;
+                break;
+            case TOP:
+                bodyTop += BEAK_LENGTH;
+                break;
+        }
 
-        ExtendedCanvas extendedCanvas = new ExtendedCanvas(200, 200);
+        ExtendedCanvas extendedCanvas = new ExtendedCanvas(right + LINE_WIDTH, bodyBottom + LINE_WIDTH);
         extendedCanvas.getElement().getStyle().setZIndex(1);
         add(extendedCanvas, 0, 0);
-        extendedCanvas.setLineWidth(2.0);
+        extendedCanvas.setLineWidth(LINE_WIDTH);
         extendedCanvas.beginPath();
         extendedCanvas.moveTo(CURVE_SIZE + bodyLeft, bodyTop);
         extendedCanvas.quadraticCurveTo(bodyLeft, bodyTop, bodyLeft, CURVE_SIZE + bodyTop);
-        leftBeakOrLine(extendedCanvas, false, left, beakOffset, bodyTop, bodyBottom, bodyLeft);
+        leftBeakOrLine(extendedCanvas, direction == Direction.LEFT, left, beakOffset, bodyTop, bodyBottom, bodyLeft);
         extendedCanvas.quadraticCurveTo(bodyLeft, bodyBottom, CURVE_SIZE + bodyLeft, bodyBottom);
-        bottomBeakOrLine(extendedCanvas, false, beakOffset, bottom, bodyLeft, bodyRight, bodyBottom);
+        bottomBeakOrLine(extendedCanvas, direction == Direction.BOTTOM, beakOffset, bottom, bodyLeft, bodyRight, bodyBottom);
         extendedCanvas.quadraticCurveTo(bodyRight, bodyBottom, bodyRight, bodyBottom - CURVE_SIZE);
-        rightBeakOrLine(extendedCanvas, true, right, beakOffset, bodyTop, bodyBottom, bodyRight);
+        rightBeakOrLine(extendedCanvas, direction == Direction.RIGHT, right, beakOffset, bodyTop, bodyBottom, bodyRight);
         extendedCanvas.quadraticCurveTo(bodyRight, bodyTop, bodyRight - CURVE_SIZE, bodyTop);
-        topBeakOrLine(extendedCanvas, false, beakOffset, top, bodyLeft, bodyRight, bodyTop);
+        topBeakOrLine(extendedCanvas, direction == Direction.TOP, beakOffset, top, bodyLeft, bodyRight, bodyTop);
 
         extendedCanvas.stroke();
         extendedCanvas.setFillStyle(Color.RED);
         extendedCanvas.fill();
-
     }
 
     private void bottomBeakOrLine(ExtendedCanvas extendedCanvas, boolean makeBeak, int beakLeft, int beakTop, int bodyLeft, int bodyRight, int bodyBottom) {
