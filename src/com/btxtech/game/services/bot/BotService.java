b213688f554b34b2fc2a39d46f8cce2bfa74a0dd
beat.keller <beat.keller@btxtech.com>
2010-03-26 21:48:18 +0000

    Bugfix bot

--- src/com/btxtech/game/services/bot/BotService.java
+++ src/com/btxtech/game/services/bot/BotService.java
@@ -25,7 +25,7 @@ import java.util.List;
 public interface BotService {
     void onHumanBaseCreated(Base base);
 
-    void onHumanBaseDefeated(Base base);
+    void onBaseDefeated(Base base);
 
     List<SimpleBase> getBotBases();
 
