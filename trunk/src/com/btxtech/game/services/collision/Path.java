adec7b70e95e431c5da66262139f4760c118d8dd
beat.keller@btxtech.com <beat.keller@btxtech.com@4948f9da-d142-11de-aacf-132551bdafba>
2010-07-04 19:32:43 +0000

    Pathfinding improved

--- trunk/src/com/btxtech/game/services/collision/Path.java
+++ trunk/src/com/btxtech/game/services/collision/Path.java
@@ -14,7 +14,6 @@
 package com.btxtech.game.services.collision;
 
 import com.btxtech.game.jsre.client.common.Rectangle;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,40 +24,35 @@ import java.util.List;
  * Time: 6:29:22 PM
  */
 public class Path {
-    private List<PassableRectangle> rectanglePath;
+    private List<PathElement> pathElements = new ArrayList<PathElement>();
 
-    public Path(Path path) {
-        rectanglePath = new ArrayList<PassableRectangle>(path.rectanglePath);
+    public Path(PassableRectangle startPassableRectangle) {
+        add(new PathElement(startPassableRectangle, 0));
     }
 
-    public Path() {
-        rectanglePath = new ArrayList<PassableRectangle>();
+    public void add(PathElement pathElement) {
+        pathElements.add(pathElement);
     }
 
-    public void add(PassableRectangle passableRectangle) {
-        rectanglePath.add(passableRectangle);
+    public PathElement getLast() {
+        return pathElements.get(pathElements.size() - 1);
     }
 
-    public boolean contains(PassableRectangle passableRectangle) {
-        return rectanglePath.contains(passableRectangle);
-    }
-
-
-    public Path createSubPath() {
-        return new Path(this);
-    }
-
-    public PassableRectangle getTail() {
-       return rectanglePath.get(rectanglePath.size() - 1);
+    public PassableRectangle getSecondLastPassableRectangle() {
+        if (pathElements.size() > 1) {
+            return pathElements.get(pathElements.size() - 2).getPassableRectangle();
+        } else {
+            return null;
+        }
     }
 
     public List<Rectangle> getAllPassableBorders() {
-        PassableRectangle previous = null;
+        PathElement previous = null;
         ArrayList<Rectangle> allBorders = new ArrayList<Rectangle>();
-        for (PassableRectangle passableRectangle : rectanglePath) {
+        for (PathElement passableRectangle : pathElements) {
             if (previous != null) {
-                Rectangle rectangle = previous.getBorder(passableRectangle);
-                if(rectangle.getWidth() > 0 && rectangle.getHeight() > 0) {
+                Rectangle rectangle = previous.getPassableRectangle().getBorder(passableRectangle.getPassableRectangle());
+                if (rectangle.getWidth() > 0 && rectangle.getHeight() > 0) {
                     throw new IllegalArgumentException("Border mus be a line");
                 }
                 allBorders.add(rectangle);
@@ -68,8 +62,26 @@ public class Path {
         return allBorders;
     }
 
-    public List<PassableRectangle> getRectanglePath() {
-        return rectanglePath;
+    public List<PathElement> getPathElements() {
+        return pathElements;
     }
-    
+
+    public boolean containsPassableRectangle(PathElement bestPathElement) {
+        for (PathElement pathElement : pathElements) {
+            if (pathElement.equalsTo(bestPathElement.getPassableRectangle())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int backToElementWithAlternatives() {
+        while (!pathElements.isEmpty()) {
+            PathElement removed = pathElements.remove(pathElements.size() - 1);
+            if (removed.isHasAlternatives()) {
+                return removed.getRank();
+            }
+        }
+        throw new IllegalStateException("Path can not be backtracked");
+    }
 }
