6ee4cf5c144fea67fbca66bcd22a73d9a2a5fd25
beat.keller <beat.keller@btxtech.com>
2010-08-09 21:41:48 +0000

    User editor added

---
+++ src/com/btxtech/game/wicket/pages/mgmt/UserEditor.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2010.
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; version 2 of the License.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ */
+
+package com.btxtech.game.wicket.pages.mgmt;
+
+import com.btxtech.game.services.user.User;
+import com.btxtech.game.services.user.UserService;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+/**
+ * User: beat
+ * Date: 09.08.2010
+ * Time: 23:04:20
+ */
+public class UserEditor extends WebPage {
+    @SpringBean
+    private UserService userService;
+
+    public UserEditor(String name) {
+        add(new FeedbackPanel("msgs"));
+
+        Form<User> form = new Form<User>("form", new CompoundPropertyModel<User>(userService.getUser(name))) {
+            @Override
+            protected void onSubmit() {
+                userService.save(getModelObject());
+            }
+        };
+        form.add(new Label("name"));
+        form.add(new TextField<Integer>("userItemTypeAccess.xp"));
+        add(form);
+
+    }
+}
---
+++ src/com/btxtech/game/wicket/pages/mgmt/UserTable.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2010.
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; version 2 of the License.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ */
+
+package com.btxtech.game.wicket.pages.mgmt;
+
+import com.btxtech.game.jsre.common.SimpleBase;
+import com.btxtech.game.services.base.BaseService;
+import com.btxtech.game.services.cms.DbCmsHomeText;
+import com.btxtech.game.services.user.User;
+import com.btxtech.game.services.user.UserService;
+import com.btxtech.game.wicket.uiservices.ListProvider;
+import java.util.List;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.CheckBox;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.data.DataView;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+/**
+ * User: beat
+ * Date: 16.02.2010
+ * Time: 21:35:44
+ */
+public class UserTable extends WebPage {
+    @SpringBean
+    private UserService userService;
+
+    public UserTable() {
+        Form form = new Form("userForm");
+        add(form);
+
+        final ListProvider<User> textProvider = new ListProvider<User>() {
+            @Override
+            protected List<User> createList() {
+                return userService.getAllUsers();
+            }
+        };
+        form.add(new DataView<User>("userTable", textProvider) {
+            @Override
+            protected void populateItem(final Item<User> item) {
+                item.add(new Label("name"));
+                item.add(new Button("edit") {
+
+                    @Override
+                    public void onSubmit() {
+                        setResponsePage(new UserEditor(item.getModelObject().getName()));
+                    }
+                });
+            }
+        });
+    }
+}
