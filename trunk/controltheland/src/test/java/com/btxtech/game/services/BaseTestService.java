9130f59ef8b682d7bb63b9936e167357c034f24f
beat.keller@btxtech.com <beat.keller@btxtech.com@4948f9da-d142-11de-aacf-132551bdafba>
2011-03-06 19:05:09 +0000

    Bugfix terrain (SVN problems)

--- trunk/controltheland/src/test/java/com/btxtech/game/services/BaseTestService.java
+++ trunk/controltheland/src/test/java/com/btxtech/game/services/BaseTestService.java
@@ -1,6 +1,9 @@
 package com.btxtech.game.services;
 
+import org.hibernate.SessionFactory;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate3.HibernateTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -10,6 +13,16 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * Time: 20:41:45
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration
+@ContextConfiguration(locations = "classpath:BaseTestService-context.xml")
 public class BaseTestService {
+    private HibernateTemplate hibernateTemplate;
+
+    @Autowired
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        hibernateTemplate = new HibernateTemplate(sessionFactory);
+    }
+
+    public HibernateTemplate getHibernateTemplate() {
+        return hibernateTemplate;
+    }
 }
