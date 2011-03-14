6266d7ad8c678ff2ea24a6cb36991807292955dd
beat.keller@btxtech.com <beat.keller@btxtech.com@4948f9da-d142-11de-aacf-132551bdafba>
2011-03-14 22:18:29 +0000

    Startup backend fixed

--- trunk/controltheland/src/main/java/com/btxtech/game/wicket/pages/mgmt/Startup.java
+++ trunk/controltheland/src/main/java/com/btxtech/game/wicket/pages/mgmt/Startup.java
@@ -44,16 +44,7 @@ public class Startup extends MgmtWebPage {
             }
         };
 
-        form.add(new TextField<String>("startMoney"));
-        form.add(new TextField<String>("tutorialTimeout"));
         form.add(new TextField<String>("registerDialogDelay"));
-        form.add(new TextField<String>("userActionCollectionTime"));
-        form.add(new TextField<String>("startRectangle.x"));
-        form.add(new TextField<String>("startRectangle.y"));
-        form.add(new TextField<String>("startRectangle.width"));
-        form.add(new TextField<String>("startRectangle.height"));
-        form.add(new TextField<String>("startItemFreeRange"));
-        form.add(new TextField<String>("itemSellFactor"));
 
         add(form);
     }
