a0522ff743ee5cab5e760a56685354de7b0a8e30
beat.keller <beat.keller@btxtech.com>
2011-04-12 09:45:30 +0000

    Bugfix sell last item

--- controltheland/src/test/java/com/btxtech/game/services/base/TestBaseService.java
+++ controltheland/src/test/java/com/btxtech/game/services/base/TestBaseService.java
@@ -1,9 +1,12 @@
 package com.btxtech.game.services.base;
 
 import com.btxtech.game.jsre.client.MovableService;
+import com.btxtech.game.jsre.client.common.Index;
+import com.btxtech.game.jsre.common.AccountBalancePacket;
 import com.btxtech.game.jsre.common.BaseChangedPacket;
 import com.btxtech.game.jsre.common.SimpleBase;
 import com.btxtech.game.jsre.common.gameengine.services.base.BaseAttributes;
+import com.btxtech.game.jsre.common.gameengine.syncObjects.Id;
 import com.btxtech.game.jsre.common.tutorial.TutorialConfig;
 import com.btxtech.game.services.BaseTestService;
 import com.btxtech.game.services.user.UserService;
@@ -22,6 +25,8 @@ public class TestBaseService extends BaseTestService {
     private UserService userService;
     @Autowired
     private MovableService movableService;
+    @Autowired
+    private BaseService baseService;
 
     @Test
     @DirtiesContext
@@ -47,4 +52,59 @@ public class TestBaseService extends BaseTestService {
         endHttpRequestAndOpenSessionInViewFilter();
         endHttpSession();
     }
+
+    @Test
+    @DirtiesContext
+    public void testSellBaseItem() throws Exception {
+        configureMinimalGame();
+
+        beginHttpSession();
+        beginHttpRequestAndOpenSessionInViewFilter();
+        Assert.assertNull(userService.getUser());
+        userService.createUser("U1", "test", "test", "test");
+        userService.login("U1", "test");
+        movableService.sendTutorialProgress(TutorialConfig.TYPE.TUTORIAL, "", "", 0, 0);
+        SimpleBase simpleBase = getMyBase(); // Setup connection
+        String color = baseService.getBaseHtmlColor(simpleBase);
+        Id id = getFirstSynItemId(simpleBase, TEST_START_BUILDER_ITEM_ID);
+        sendBuildCommand(id, new Index(400, 400), TEST_FACTORY_ITEM_ID);
+        waitForActionServiceDone();
+        movableService.sellItem(id);
+
+        AccountBalancePacket accountBalancePacket = new AccountBalancePacket();
+        accountBalancePacket.setAccountBalance(998);
+        assertPackagesIgnoreSyncItemInfoAndClear(simpleBase, accountBalancePacket);
+
+        endHttpRequestAndOpenSessionInViewFilter();
+        endHttpSession();
+    }
+
+    @Test
+    @DirtiesContext
+    public void testSellLastBaseItem() throws Exception {
+        configureMinimalGame();
+
+        beginHttpSession();
+        beginHttpRequestAndOpenSessionInViewFilter();
+        Assert.assertNull(userService.getUser());
+        userService.createUser("U1", "test", "test", "test");
+        userService.login("U1", "test");
+        movableService.sendTutorialProgress(TutorialConfig.TYPE.TUTORIAL, "", "", 0, 0);
+        SimpleBase simpleBase = getMyBase(); // Setup connection
+        String color = baseService.getBaseHtmlColor(simpleBase);
+        Id id = getFirstSynItemId(simpleBase, TEST_START_BUILDER_ITEM_ID);
+        movableService.sellItem(id);
+
+        Thread.sleep(1000);
+
+        BaseChangedPacket baseChangedPacket = new BaseChangedPacket();
+        baseChangedPacket.setType(BaseChangedPacket.Type.REMOVED);
+        baseChangedPacket.setBaseAttributes(new BaseAttributes(simpleBase, "U1", color, false));
+        assertPackagesIgnoreSyncItemInfoAndClear(simpleBase, baseChangedPacket);
+
+
+        endHttpRequestAndOpenSessionInViewFilter();
+        endHttpSession();
+    }
+
 }
