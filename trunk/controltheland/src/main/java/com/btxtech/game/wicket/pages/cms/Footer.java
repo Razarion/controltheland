4b320cb3fa187a91f7357702ae958531a1c7678c
beat.keller@btxtech.com <beat.keller@btxtech.com@4948f9da-d142-11de-aacf-132551bdafba>
2011-07-01 22:37:27 +0000

    CMS start paged now possible 3

---
+++ trunk/controltheland/src/main/java/com/btxtech/game/wicket/pages/cms/Footer.java
@@ -0,0 +1,27 @@
+package com.btxtech.game.wicket.pages.cms;
+
+import com.btxtech.game.services.cms.DbPage;
+import com.btxtech.game.wicket.pages.user.LoggedinBox;
+import com.btxtech.game.wicket.pages.user.LoginBox;
+import org.apache.wicket.markup.html.panel.Panel;
+
+/**
+ * User: beat
+ * Date: 01.07.2011
+ * Time: 23:33:55
+ */
+public class Footer extends Panel {
+    private boolean footerVisible;
+
+    public Footer(String id, DbPage dbPage) {
+        super(id);
+        footerVisible = dbPage.isFooterVisible();
+        add(new LoggedinBox("loggedinBox"));
+        add(new LoginBox("loginBox", true));
+    }
+
+    @Override
+    public boolean isVisible() {
+        return footerVisible;
+    }
+}
