d59863490151c8b86cbac9915935ed6e7015c334
beat.keller@btxtech.com <beat.keller@btxtech.com@4948f9da-d142-11de-aacf-132551bdafba>
2011-07-29 21:58:45 +0000

    CMS bugfix

--- trunk/controltheland/src/main/java/com/btxtech/game/wicket/pages/cms/content/ContentContainer.java
+++ trunk/controltheland/src/main/java/com/btxtech/game/wicket/pages/cms/content/ContentContainer.java
@@ -5,13 +5,9 @@ import com.btxtech.game.services.cms.DbContentContainer;
 import com.btxtech.game.wicket.uiservices.BeanIdPathElement;
 import com.btxtech.game.wicket.uiservices.cms.CmsUiService;
 import org.apache.wicket.behavior.SimpleAttributeModifier;
-import org.apache.wicket.markup.html.list.ListItem;
-import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.model.LoadableDetachableModel;
+import org.apache.wicket.markup.repeater.RepeatingView;
 import org.apache.wicket.spring.injection.annot.SpringBean;
-
-import java.util.List;
 
 /**
  * User: beat
@@ -21,49 +17,27 @@ import java.util.List;
 public class ContentContainer extends Panel {
     @SpringBean
     private CmsUiService cmsUiService;
-    private Object bean;
     private BeanIdPathElement beanIdPathElement;
     private int contentId;
 
-    public ContentContainer(String id, DbContentContainer dbContentContentContainer, final BeanIdPathElement beanIdPathElement) {
+    public ContentContainer(String id, DbContentContainer dbContentContainer, final BeanIdPathElement beanIdPathElement) {
         super(id);
         this.beanIdPathElement = beanIdPathElement;
-        contentId = dbContentContentContainer.getId();
-        setDefaultModel(new LoadableDetachableModel<DbContentContainer>() {
-            @Override
-            protected DbContentContainer load() {
-                bean = cmsUiService.getDataProviderBean(beanIdPathElement);
-                return cmsUiService.getDbContent(contentId);
-            }
-
-            @Override
-            protected void onDetach() {
-                bean = null;
-            }
-        });
-        setupPropertyBook();
-        if (dbContentContentContainer.getCssClass() != null) {
-            add(new SimpleAttributeModifier("class", dbContentContentContainer.getCssClass()));
+        contentId = dbContentContainer.getId();
+        setupContainer(dbContentContainer);
+        if (dbContentContainer.getCssClass() != null) {
+            add(new SimpleAttributeModifier("class", dbContentContainer.getCssClass()));
         }
     }
 
-    private void setupPropertyBook() {
-        ListView<DbContent> listView = new ListView<DbContent>("listView", new LoadableDetachableModel<List<DbContent>>() {
-
-            @Override
-            protected List<DbContent> load() {
-                DbContentContainer dbContentContentContainer = (DbContentContainer) getDefaultModelObject();
-                return dbContentContentContainer.getContentCrud().readDbChildren();
-            }
-        }) {
-
-            @Override
-            protected void populateItem(ListItem<DbContent> dbContentListItem) {
-                BeanIdPathElement childBeanIdPathElement = cmsUiService.createChildBeanIdPathElement(dbContentListItem.getModelObject(), beanIdPathElement, null);
-                dbContentListItem.add(cmsUiService.getComponent(dbContentListItem.getModelObject(), bean, "content", childBeanIdPathElement));
-            }
-        };
-        add(listView);
+    private void setupContainer(DbContentContainer dbContentContainer) {
+        Object bean = cmsUiService.getDataProviderBean(beanIdPathElement);
+        RepeatingView view = new RepeatingView("container");
+        for (DbContent dbContent : dbContentContainer.getContentCrud().readDbChildren()) {
+            BeanIdPathElement childBeanIdPathElement = cmsUiService.createChildBeanIdPathElement(dbContent, beanIdPathElement, null);
+            view.add(cmsUiService.getComponent(dbContent, bean, view.newChildId(), childBeanIdPathElement));
+        }
+        add(view);
     }
 
     @Override
