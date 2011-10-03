892d6f367a9265638df5fee80bee705177a13dcf
beat.keller <beat.keller@btxtech.com>
2011-10-03 14:20:42 +0000

    Client exception handling improved

--- controltheland/src/main/java/com/btxtech/game/services/gwt/ExtendedRemoteLoggingServiceImpl.java
+++ controltheland/src/main/java/com/btxtech/game/services/gwt/ExtendedRemoteLoggingServiceImpl.java
@@ -1,5 +1,6 @@
 package com.btxtech.game.services.gwt;
 
+import com.btxtech.game.services.common.Utils;
 import com.google.gwt.logging.server.RemoteLoggingServiceImpl;
 
 import javax.servlet.ServletConfig;
@@ -11,13 +12,20 @@ import javax.servlet.ServletException;
  * Time: 16:37:37
  */
 public class ExtendedRemoteLoggingServiceImpl extends RemoteLoggingServiceImpl {
-    private static final String SYMBOL_PARAMETER_MAP = "symbolMapsDirectory";
+    private static final String SYMBOL_PARAMETER_MAP_PROD = "symbolMapsDirectoryProd";
+    private static final String SYMBOL_PARAMETER_MAP_DEV = "symbolMapsDirectoryDev";
 
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
-        if (getInitParameter(SYMBOL_PARAMETER_MAP) != null && !getInitParameter(SYMBOL_PARAMETER_MAP).trim().isEmpty()) {
-            setSymbolMapsDirectory(getInitParameter(SYMBOL_PARAMETER_MAP));
+        if (Utils.isTestModeStatic()) {
+            if (getInitParameter(SYMBOL_PARAMETER_MAP_DEV) != null && !getInitParameter(SYMBOL_PARAMETER_MAP_DEV).trim().isEmpty()) {
+                setSymbolMapsDirectory(config.getServletContext().getRealPath(getInitParameter(SYMBOL_PARAMETER_MAP_DEV)));
+            }
+        } else {
+            if (getInitParameter(SYMBOL_PARAMETER_MAP_PROD) != null && !getInitParameter(SYMBOL_PARAMETER_MAP_PROD).trim().isEmpty()) {
+                setSymbolMapsDirectory(config.getServletContext().getRealPath(getInitParameter(SYMBOL_PARAMETER_MAP_PROD)));
+            }
         }
     }
 }
