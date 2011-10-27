0c61ba0461a76bb8a4d70d0796fdb127341b08fb
beat.keller@btxtech.com <beat.keller@btxtech.com@4948f9da-d142-11de-aacf-132551bdafba>
2011-10-27 22:46:17 +0000

    HTTP expires set

--- trunk/controltheland/src/main/java/com/btxtech/game/controllers/ItemImageController.java
+++ trunk/controltheland/src/main/java/com/btxtech/game/controllers/ItemImageController.java
@@ -14,20 +14,20 @@
 package com.btxtech.game.controllers;
 
 import com.btxtech.game.jsre.client.common.Constants;
+import com.btxtech.game.services.common.DateUtil;
 import com.btxtech.game.services.item.ItemService;
 import com.btxtech.game.services.item.itemType.DbItemTypeImage;
-
-import java.io.IOException;
-import java.io.OutputStream;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
 
 @Component(value = "itemImageController")
 public class ItemImageController implements Controller {
@@ -38,15 +38,15 @@ public class ItemImageController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception {
         try {
-        int itemTypeId = Integer.parseInt(httpServletRequest.getParameter(Constants.ITEM_IMAGE_ID));
-        int index = Integer.parseInt(httpServletRequest.getParameter(Constants.ITEM_IMAGE_INDEX));
-
-        DbItemTypeImage itemTypeImage = itemService.getItemTypeImage(itemTypeId, index);
-        httpServletResponse.setContentLength(itemTypeImage.getData().length);
-        httpServletResponse.setContentType(itemTypeImage.getContentType());
-        OutputStream out = httpServletResponse.getOutputStream();
-        out.write(itemTypeImage.getData());
-        out.close();
+            int itemTypeId = Integer.parseInt(httpServletRequest.getParameter(Constants.ITEM_IMAGE_ID));
+            int index = Integer.parseInt(httpServletRequest.getParameter(Constants.ITEM_IMAGE_INDEX));
+            DbItemTypeImage itemTypeImage = itemService.getItemTypeImage(itemTypeId, index);
+            httpServletResponse.setContentLength(itemTypeImage.getData().length);
+            httpServletResponse.setContentType(itemTypeImage.getContentType());
+            httpServletResponse.addDateHeader("Expires", System.currentTimeMillis() + DateUtil.MILLIS_IN_DAY);
+            OutputStream out = httpServletResponse.getOutputStream();
+            out.write(itemTypeImage.getData());
+            out.close();
         } catch (IOException e) {
             // Connection lost -> ignore
         } catch (Exception e) {
