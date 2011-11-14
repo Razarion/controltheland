2c3bd5c8cf54f78a3a6a52b0ff1af18b092d1307
beat.keller@btxtech.com <beat.keller@btxtech.com@4948f9da-d142-11de-aacf-132551bdafba>
2011-11-14 11:17:02 +0000

    SideCockpit: tooltip & icons

--- trunk/controltheland/src/main/java/com/btxtech/game/jsre/client/cockpit/RadarControlPanel.java
+++ trunk/controltheland/src/main/java/com/btxtech/game/jsre/client/cockpit/RadarControlPanel.java
@@ -1,6 +1,7 @@
 package com.btxtech.game.jsre.client.cockpit;
 
 import com.btxtech.game.jsre.client.cockpit.radar.RadarPanel;
+import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.Widget;
 
 /**
@@ -8,13 +9,15 @@ import com.google.gwt.user.client.ui.Widget;
  * Date: 07.11.2011
  * Time: 20:11:19
  */
-public class RadarControlPanel extends AbstractControlPanel{
+public class RadarControlPanel extends AbstractControlPanel {
     public RadarControlPanel(int width, int height) {
         super(width, height);
     }
 
     @Override
     protected Widget createBody() {
-        return RadarPanel.getInstance().createWidget(getContentWidth(), getContentHeight());
+        AbsolutePanel absolutePanel = RadarPanel.getInstance().createWidget(getContentWidth(), getContentHeight());
+        absolutePanel.setTitle(ToolTips.TOOL_TIP_RADAR);
+        return absolutePanel;
     }
 }
---
+++ trunk/controltheland/src/main/java/com/btxtech/game/jsre/client/cockpit/ToolTips.java
@@ -0,0 +1,19 @@
+package com.btxtech.game.jsre.client.cockpit;
+
+/**
+ * User: beat
+ * Date: 13.11.2011
+ * Time: 23:30:20
+ */
+public class ToolTips {
+    public static final String TOOL_TIP_RADAR = "Overview of the entire map. Click on the minimap to navigate";
+    public static final String TOOL_TIP_LEVEL_TARGET = "Shows level target to move to the next level";
+    public static final String TOOL_TIP_LEVEL = "Your current level";
+    public static final String TOOL_TIP_MONEY = "Your money";
+
+    // Unused
+    public static final String TOOL_TIP_SCROLL_HOME = "Scroll home";
+    public static final String TOOL_TIP_SELL = "Activate sell mode for selling units and structures";
+    public static final String TOOL_TIP_ENERGY = "Base energy producing / consuming";
+    public static final String TOOL_TIP_UNITS = "Units amount / max";
+}
