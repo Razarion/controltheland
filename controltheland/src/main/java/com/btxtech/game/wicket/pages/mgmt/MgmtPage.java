c1938a98077592a1a82221b5ec74f1db6a781945
beat.keller <beat.keller@btxtech.com>
2011-12-22 23:05:23 +0000

    Bugfix

--- controltheland/src/main/java/com/btxtech/game/wicket/pages/mgmt/MgmtPage.java
+++ controltheland/src/main/java/com/btxtech/game/wicket/pages/mgmt/MgmtPage.java
@@ -119,7 +119,6 @@ public class MgmtPage extends MgmtWebPage {
     static {
         toolPages = new ArrayList<LinkAndName>();
         toolPages.add(new LinkAndName("Startup", Startup.class));
-        toolPages.add(new LinkAndName("CMS", CmsEditor.class));
         toolPages.add(new LinkAndName("DB View", DbView.class));
         toolPages.add(new LinkAndName("Log View", LogView.class));
         toolPages.add(new LinkAndName("Map Editor", TerrainSettingsTable.class));
