8073f876d8bd28b30d2d6986a74b44b7ecc8a47c
beat.keller@btxtech.com <beat.keller@btxtech.com@4948f9da-d142-11de-aacf-132551bdafba>
2012-03-23 16:58:28 +0000

    Statistics: End of day processing removed -> loop Bug pasted for later usage

--- trunk/controltheland/src/main/java/com/btxtech/game/services/statistics/impl/StatisticsServiceImpl.java
+++ trunk/controltheland/src/main/java/com/btxtech/game/services/statistics/impl/StatisticsServiceImpl.java
@@ -51,7 +51,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 /**
  * User: beat
@@ -81,6 +80,177 @@ public class StatisticsServiceImpl implements StatisticsService {
     private long nextDay;
     private Log log = LogFactory.getLog(StatisticsServiceImpl.class);
 
+    // Bugs
+    // 18.09.2011	Statistics wird auf DB nicht augeräumt. Alte Einträge bleiben erhalten.
+    // 04.10.2011  More than one entry in all time statistics for user: User: 'beat'
+    // 22.09.2011	Kill Total, Kill Bot Total, Kill Player Total
+    // 22.09.2011	Lost (Bot, Player, Units, Structure, All)
+    // 01.11.2011  End of Day runs more than 100 times (30.10.2011)
+    // 20.11.2011  More than one entry in all time statistics for user
+    // 20.11.2011  Loop in end of day processing
+    // 17.02.2012  00:12:45.656  INFO Start end of day processing
+    // 17.02.2012  00:12:45.656 ERROR org.hibernate.service.UnknownServiceException: Unknown service requested [org.hibernate.service.jdbc.connections.spi.ConnectionProvider]
+    // ------------------------------------------------------------------------------------------------
+    // 17.02.2012 00:12:42.659 ERROR
+    // java.lang.NullPointerException
+    // 17.02.2012 00:12:42.659  INFO Finished end of day processing
+    // 17.02.2012 00:12:42.659  INFO Start end of day processing
+    // ------------------------------------------------------------------------------------------------
+    // java.lang.NullPointerException
+    // 20.11.2011 11:37:36.606  INFO Finished end of day processing
+    // 20.11.2011 11:37:36.606  INFO Start end of day processing
+    // 20.11.2011 11:37:36.609 ERROR
+    // ------------------------------------------------------------------------------------------------
+    // 27.01.2011	After Prod shutdown:
+    // "StatisticsServiceImpl Thread 1" prio=10 tid=0x00007f3b745d8800 nid=0xf8e waiting on condition [0x00007f3b82180000]
+    //    java.lang.Thread.State: WAITING (parking)
+    //         at sun.misc.Unsafe.park(Native Method)
+    //         - parking to wait for  <0x0000000758f8a198> (a java.util.concurrent.locks.AbstractQueuedSynchronizer$ConditionObject)
+    //         at java.util.concurrent.locks.LockSupport.park(LockSupport.java:186)
+    //         at java.util.concurrent.locks.AbstractQueuedSynchronizer$ConditionObject.await(AbstractQueuedSynchronizer.java:2043)
+    //         at java.util.concurrent.DelayQueue.take(DelayQueue.java:189)
+    //         at java.util.concurrent.ScheduledThreadPoolExecutor$DelayedWorkQueue.take(ScheduledThreadPoolExecutor.java:688)
+    //         at java.util.concurrent.ScheduledThreadPoolExecutor$DelayedWorkQueue.take(ScheduledThreadPoolExecutor.java:681)
+    //         at java.util.concurrent.ThreadPoolExecutor.getTask(ThreadPoolExecutor.java:1043)
+    //         at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1103)
+    //         at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:603)
+    //         at java.lang.Thread.run(Thread.java:636)
+    // ------------------------------------------------------------------------------------------------
+    // 17.02.2012 00:11:57.311  INFO Start end of day processing
+    // 17.02.2012 00:11:57.323  WARN SQL Error: 0, SQLState: 08S01
+    // 17.02.2012 00:11:57.323 ERROR Communications link failure
+//    The last packet sent successfully to the server was 0 milliseconds ago. The driver has not received any packets from the server.
+//    17.02.2012 00:11:57.323 ERROR
+//    org.springframework.transaction.CannotCreateTransactionException: Could not open Hibernate Session for transaction; nested exception is org.
+//    hibernate.exception.JDBCConnectionException: Could not open connection
+//            at org.springframework.orm.hibernate4.HibernateTransactionManager.doBegin(HibernateTransactionManager.java:440)
+//            at org.springframework.transaction.support.AbstractPlatformTransactionManager.getTransaction(AbstractPlatformTransactionManager.java
+//    :371)
+//            at org.springframework.transaction.support.TransactionTemplate.execute(TransactionTemplate.java:127)
+//            at com.btxtech.game.services.statistics.impl.StatisticsServiceImpl.endOfDayProcessing(StatisticsServiceImpl.java:153)
+//            at com.btxtech.game.services.statistics.impl.StatisticsServiceImpl$1.run(StatisticsServiceImpl.java:104)
+//            at java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:471)
+//            at java.util.concurrent.FutureTask$Sync.innerRun(FutureTask.java:334)
+//            at java.util.concurrent.FutureTask.run(FutureTask.java:166)
+//            at java.util.concurrent.ScheduledThreadPoolExecutor$ScheduledFutureTask.access$101(ScheduledThreadPoolExecutor.java:165)
+//            at java.util.concurrent.ScheduledThreadPoolExecutor$ScheduledFutureTask.run(ScheduledThreadPoolExecutor.java:266)
+//            at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1110)
+//            at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:603)
+//            at java.lang.Thread.run(Thread.java:636)
+//    Caused by: org.hibernate.exception.JDBCConnectionException: Could not open connection
+//            at org.hibernate.exception.internal.SQLStateConverter.convert(SQLStateConverter.java:107)
+//            at org.hibernate.engine.jdbc.spi.SqlExceptionHelper.convert(SqlExceptionHelper.java:125)
+//            at org.hibernate.engine.jdbc.spi.SqlExceptionHelper.convert(SqlExceptionHelper.java:110)
+//            at org.hibernate.engine.jdbc.internal.LogicalConnectionImpl.obtainConnection(LogicalConnectionImpl.java:304)
+//            at org.hibernate.engine.jdbc.internal.LogicalConnectionImpl.getConnection(LogicalConnectionImpl.java:169)
+//            at org.hibernate.engine.transaction.internal.jdbc.JdbcTransaction.doBegin(JdbcTransaction.java:67)
+//            at org.hibernate.engine.transaction.spi.AbstractTransactionImpl.begin(AbstractTransactionImpl.java:160)
+//            at org.hibernate.internal.SessionImpl.beginTransaction(SessionImpl.java:1263)
+//            at org.springframework.orm.hibernate4.HibernateTransactionManager.doBegin(HibernateTransactionManager.java:399)
+//            ... 12 more
+//    Caused by: com.mysql.jdbc.exceptions.jdbc4.CommunicationsException: Communications link failure
+//
+//    The last packet sent successfully to the server was 0 milliseconds ago. The driver has not received any packets from the server.
+//            at sun.reflect.GeneratedConstructorAccessor1016.newInstance(Unknown Source)
+//            at sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)
+//            at java.lang.reflect.Constructor.newInstance(Constructor.java:532)
+//            at com.mysql.jdbc.Util.handleNewInstance(Util.java:407)
+//            at com.mysql.jdbc.SQLError.createCommunicationsException(SQLError.java:1116)
+//            at com.mysql.jdbc.MysqlIO.<init>(MysqlIO.java:343)
+//            at com.mysql.jdbc.ConnectionImpl.coreConnect(ConnectionImpl.java:2334)
+//            at com.mysql.jdbc.ConnectionImpl.connectOneTryOnly(ConnectionImpl.java:2371)
+//            at com.mysql.jdbc.ConnectionImpl.createNewIO(ConnectionImpl.java:2163)
+//            at com.mysql.jdbc.ConnectionImpl.<init>(ConnectionImpl.java:794)
+//            at com.mysql.jdbc.JDBC4Connection.<init>(JDBC4Connection.java:47)
+//            at sun.reflect.GeneratedConstructorAccessor916.newInstance(Unknown Source)
+//            at sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)
+//            at java.lang.reflect.Constructor.newInstance(Constructor.java:532)
+//            at com.mysql.jdbc.Util.handleNewInstance(Util.java:407)
+//            at com.mysql.jdbc.ConnectionImpl.getInstance(ConnectionImpl.java:378)
+//            at com.mysql.jdbc.NonRegisteringDriver.connect(NonRegisteringDriver.java:305)
+//            at java.sql.DriverManager.getConnection(DriverManager.java:620)
+//            at java.sql.DriverManager.getConnection(DriverManager.java:169)
+//            at org.springframework.jdbc.datasource.DriverManagerDataSource.getConnectionFromDriverManager(DriverManagerDataSource.java:173)
+//            at org.springframework.jdbc.datasource.DriverManagerDataSource.getConnectionFromDriver(DriverManagerDataSource.java:164)
+//            at org.springframework.jdbc.datasource.AbstractDriverBasedDataSource.getConnectionFromDriver(AbstractDriverBasedDataSource.java:149)
+//            at org.springframework.jdbc.datasource.AbstractDriverBasedDataSource.getConnection(AbstractDriverBasedDataSource.java:119)
+//            at org.hibernate.service.jdbc.connections.internal.DatasourceConnectionProviderImpl.getConnection(DatasourceConnectionProviderImpl.j
+//    ava:141)
+//            at org.hibernate.internal.AbstractSessionImpl$NonContextualJdbcConnectionAccess.obtainConnection(AbstractSessionImpl.java:276)
+//            at org.hibernate.engine.jdbc.internal.LogicalConnectionImpl.obtainConnection(LogicalConnectionImpl.java:297)
+//            ... 17 more
+//    Caused by: java.net.ConnectException: Connection refused
+//            at java.net.PlainSocketImpl.socketConnect(Native Method)
+//            at java.net.AbstractPlainSocketImpl.doConnect(AbstractPlainSocketImpl.java:327)
+//            at java.net.AbstractPlainSocketImpl.connectToAddress(AbstractPlainSocketImpl.java:193)
+//            at java.net.AbstractPlainSocketImpl.connect(AbstractPlainSocketImpl.java:180)
+//            at java.net.SocksSocketImpl.connect(SocksSocketImpl.java:384)
+//            at java.net.Socket.connect(Socket.java:546)
+//            at java.net.Socket.connect(Socket.java:495)
+//            at java.net.Socket.<init>(Socket.java:392)
+//            at java.net.Socket.<init>(Socket.java:235)
+//            at com.mysql.jdbc.StandardSocketFactory.connect(StandardSocketFactory.java:254)
+//            at com.mysql.jdbc.MysqlIO.<init>(MysqlIO.java:292)
+//            ... 37 more
+//    17.02.2012 00:11:57.324  INFO Finished end of day processing
+//    ------------------------------------------------------------------------------------------------
+//    28.02.2012 07:45:18.271 ERROR
+//    java.lang.NullPointerException
+//        at com.btxtech.game.controllers.StatisticsController.handleRequest(StatisticsController.java:39)
+//        at org.springframework.web.servlet.mvc.SimpleControllerHandlerAdapter.handle(SimpleControllerHandlerAdapter.java:48)
+//        at org.springframework.web.servlet.DispatcherServlet.doDispatch(DispatcherServlet.java:900)
+//        at org.springframework.web.servlet.DispatcherServlet.doService(DispatcherServlet.java:827)
+//        at org.springframework.web.servlet.FrameworkServlet.processRequest(FrameworkServlet.java:882)
+//        at org.springframework.web.servlet.FrameworkServlet.doGet(FrameworkServlet.java:778)
+//        at javax.servlet.http.HttpServlet.service(HttpServlet.java:617)
+//        at javax.servlet.http.HttpServlet.service(HttpServlet.java:717)
+//        at org.apache.catalina.core.ApplicationFilterChain.internalDoFilter(ApplicationFilterChain.java:290)
+//        at org.apache.catalina.core.ApplicationFilterChain.doFilter(ApplicationFilterChain.java:206)
+//        at org.apache.wicket.protocol.http.WicketFilter.doFilter(WicketFilter.java:370)
+//        at org.apache.catalina.core.ApplicationFilterChain.internalDoFilter(ApplicationFilterChain.java:235)
+//        at org.apache.catalina.core.ApplicationFilterChain.doFilter(ApplicationFilterChain.java:206)
+//        at org.springframework.orm.hibernate4.support.OpenSessionInViewFilter.doFilterInternal(OpenSessionInViewFilter.java:119)
+//        at org.springframework.web.filter.OncePerRequestFilter.doFilter(OncePerRequestFilter.java:76)
+//        at org.apache.catalina.core.ApplicationFilterChain.internalDoFilter(ApplicationFilterChain.java:235)
+//        at org.apache.catalina.core.ApplicationFilterChain.doFilter(ApplicationFilterChain.java:206)
+//        at org.springframework.security.web.context.SecurityContextPersistenceFilter.doFilter(SecurityContextPersistenceFilter.java:87)
+//        at org.apache.catalina.core.ApplicationFilterChain.internalDoFilter(ApplicationFilterChain.java:235)
+//        at org.apache.catalina.core.ApplicationFilterChain.doFilter(ApplicationFilterChain.java:206)
+//        at org.apache.catalina.core.StandardWrapperValve.invoke(StandardWrapperValve.java:233)
+//        at org.apache.catalina.core.StandardContextValve.invoke(StandardContextValve.java:191)
+//        at org.apache.catalina.core.StandardHostValve.invoke(StandardHostValve.java:128)
+//        at org.apache.catalina.valves.ErrorReportValve.invoke(ErrorReportValve.java:102)
+//        at org.apache.catalina.core.StandardEngineValve.invoke(StandardEngineValve.java:109)
+//        at org.apache.catalina.connector.CoyoteAdapter.service(CoyoteAdapter.java:293)
+//        at org.apache.coyote.http11.Http11Processor.process(Http11Processor.java:849)
+//        at org.apache.coyote.http11.Http11Protocol$Http11ConnectionHandler.process(Http11Protocol.java:583)
+//        at org.apache.tomcat.util.net.JIoEndpoint$Worker.run(JIoEndpoint.java:454)
+//        at java.lang.Thread.run(Thread.java:636)
+//    ------------------------------------------------------------------------------------------------
+//    Loop!!! End of day processing
+//    21.03.2012 00:00:09.945  INFO Finished end of day processing
+//    21.03.2012 00:00:09.945  INFO Start end of day processing
+//    21.03.2012 00:00:09.947 ERROR
+//    java.lang.NullPointerException
+//            at com.btxtech.game.services.statistics.impl.StatisticsServiceImpl.moveCacheToDb(StatisticsServiceImpl.java:149)
+//            at com.btxtech.game.services.statistics.impl.StatisticsServiceImpl.closeDayIfNecessary(StatisticsServiceImpl.java:408)
+//            at com.btxtech.game.services.statistics.impl.StatisticsServiceImpl.access$300(StatisticsServiceImpl.java:62)
+//            at com.btxtech.game.services.statistics.impl.StatisticsServiceImpl$2.doInTransactionWithoutResult(StatisticsServiceImpl.java:170)
+//            at org.springframework.transaction.support.TransactionCallbackWithoutResult.doInTransaction(TransactionCallbackWithoutResult.java:33)
+//            at org.springframework.transaction.support.TransactionTemplate.execute(TransactionTemplate.java:130)
+//            at com.btxtech.game.services.statistics.impl.StatisticsServiceImpl.endOfDayProcessing(StatisticsServiceImpl.java:167)
+//            at com.btxtech.game.services.statistics.impl.StatisticsServiceImpl$1.run(StatisticsServiceImpl.java:104)
+//            at java.util.concurrent.Executors$RunnableAdapter.call(Unknown Source)
+//            at java.util.concurrent.FutureTask$Sync.innerRun(Unknown Source)
+//            at java.util.concurrent.FutureTask.run(Unknown Source)
+//            at java.util.concurrent.ScheduledThreadPoolExecutor$ScheduledFutureTask.access$201(Unknown Source)
+//            at java.util.concurrent.ScheduledThreadPoolExecutor$ScheduledFutureTask.run(Unknown Source)
+//            at java.util.concurrent.ThreadPoolExecutor.runWorker(Unknown Source)
+//            at java.util.concurrent.ThreadPoolExecutor$Worker.run(Unknown Source)
+//            at java.lang.Thread.run(Unknown Source)
+//    ------------------------------------------------------------------------------------------------
+
+
     @PostConstruct
     public void init() {
         dayStatistics.init(DbStatisticsEntry.class);
@@ -96,7 +266,7 @@ public class StatisticsServiceImpl implements StatisticsService {
     }
 
     private void scheduleNextEndDayProcessing() {
-        scheduledThreadPoolExecutor.schedule(new Runnable() {
+        /*scheduledThreadPoolExecutor.schedule(new Runnable() {
             @Override
             public void run() {
                 try {
@@ -113,6 +283,7 @@ public class StatisticsServiceImpl implements StatisticsService {
                 }
             }
         }, nextDay - System.currentTimeMillis(), TimeUnit.MILLISECONDS);
+        */
     }
 
     private void setupNextDay() {
