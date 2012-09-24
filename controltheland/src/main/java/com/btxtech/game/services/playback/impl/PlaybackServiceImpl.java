8a9d207a620294d99de256e2084fb6c9120ad1d3
beat.keller <beat.keller@btxtech.com>
2012-09-24 21:34:09 +0000

    Playback fixed

--- controltheland/src/main/java/com/btxtech/game/services/playback/impl/PlaybackServiceImpl.java
+++ controltheland/src/main/java/com/btxtech/game/services/playback/impl/PlaybackServiceImpl.java
@@ -13,20 +13,38 @@
 
 package com.btxtech.game.services.playback.impl;
 
+import com.btxtech.game.jsre.common.packets.SyncItemInfo;
+import com.btxtech.game.jsre.common.utg.tracking.BrowserWindowTracking;
+import com.btxtech.game.jsre.common.utg.tracking.DialogTracking;
+import com.btxtech.game.jsre.common.utg.tracking.EventTrackingItem;
+import com.btxtech.game.jsre.common.utg.tracking.SelectionTrackingItem;
+import com.btxtech.game.jsre.common.utg.tracking.TerrainScrollTracking;
 import com.btxtech.game.jsre.playback.Playback;
 import com.btxtech.game.jsre.playback.PlaybackInfo;
 import com.btxtech.game.services.gwt.AutowiredRemoteServiceServlet;
+import com.btxtech.game.services.gwt.MovableServiceImpl;
 import com.btxtech.game.services.item.ServerItemTypeService;
 import com.btxtech.game.services.mgmt.MgmtService;
 import com.btxtech.game.services.sound.SoundService;
+import com.btxtech.game.services.terrain.TerrainDbUtil;
 import com.btxtech.game.services.terrain.TerrainImageService;
+import com.btxtech.game.services.tutorial.DbTutorialConfig;
 import com.btxtech.game.services.tutorial.TutorialService;
 import com.btxtech.game.services.user.UserService;
+import com.btxtech.game.services.utg.LifecycleTrackingInfo;
 import com.btxtech.game.services.utg.UserTrackingService;
+import com.btxtech.game.services.utg.tracker.DbBrowserWindowTracking;
+import com.btxtech.game.services.utg.tracker.DbDialogTracking;
+import com.btxtech.game.services.utg.tracker.DbEventTrackingItem;
+import com.btxtech.game.services.utg.tracker.DbScrollTrackingItem;
+import com.btxtech.game.services.utg.tracker.DbSelectionTrackingItem;
+import com.btxtech.game.services.utg.tracker.DbSyncItemInfo;
 import com.btxtech.game.wicket.uiservices.cms.CmsUiService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
 
 /**
  * User: beat
@@ -37,7 +55,7 @@ public class PlaybackServiceImpl extends AutowiredRemoteServiceServlet implement
     @Autowired
     private UserTrackingService userTrackingService;
     @Autowired
-    private TerrainImageService terrainService;
+    private TerrainImageService terrainImageService;
     @Autowired
     private TutorialService tutorialService;
     @Autowired
@@ -54,8 +72,6 @@ public class PlaybackServiceImpl extends AutowiredRemoteServiceServlet implement
 
     @Override
     public PlaybackInfo getPlaybackInfo(String startUuid) {
-        throw new UnsupportedOperationException();
-        /*
         try {
             PlaybackInfo playbackInfo = new PlaybackInfo();
 
@@ -63,48 +79,49 @@ public class PlaybackServiceImpl extends AutowiredRemoteServiceServlet implement
             MovableServiceImpl.setCommonInfo(playbackInfo, userService, serverItemTypeService, mgmtService, cmsUiService, soundService);
             LifecycleTrackingInfo lifecycleTrackingInfo = userTrackingService.getLifecycleTrackingInfo(startUuid);
             DbTutorialConfig dbTutorialConfig = tutorialService.getDbTutorialConfig4Tracking(lifecycleTrackingInfo.getLevelTaskId());
-            // TODO playbackInfo.setTutorialConfig(dbTutorialConfig.getTutorialConfig(serverItemTypeService));
-            terrainService.setupTerrainTutorial(playbackInfo, dbTutorialConfig);
+            playbackInfo.setTutorialConfig(dbTutorialConfig.getTutorialConfig(serverItemTypeService));
+            TerrainDbUtil.loadTerrainFromDb(dbTutorialConfig.getDbTerrainSetting(), playbackInfo);
+            terrainImageService.setupTerrainImages(playbackInfo);
 
             playbackInfo.setEventTrackingStart(userTrackingService.getDbEventTrackingStart(startUuid).createEventTrackingStart());
 
             // Mouse tracking
-            ArrayList<EventTrackingItem> eventTrackingItems = new ArrayList<EventTrackingItem>();
+            ArrayList<EventTrackingItem> eventTrackingItems = new ArrayList<>();
             for (DbEventTrackingItem dbEventTrackingItem : userTrackingService.getDbEventTrackingItem(startUuid)) {
                 eventTrackingItems.add(dbEventTrackingItem.createEventTrackingItem());
             }
             playbackInfo.setEventTrackingItems(eventTrackingItems);
 
             // Selections
-            ArrayList<SelectionTrackingItem> selectionTrackingItems = new ArrayList<SelectionTrackingItem>();
+            ArrayList<SelectionTrackingItem> selectionTrackingItems = new ArrayList<>();
             for (DbSelectionTrackingItem dbSelectionTrackingItem : userTrackingService.getDbSelectionTrackingItems(startUuid)) {
                 selectionTrackingItems.add(dbSelectionTrackingItem.createSelectionTrackingItem());
             }
             playbackInfo.setSelectionTrackingItems(selectionTrackingItems);
 
             // Commands
-            ArrayList<SyncItemInfo> syncItemInfos = new ArrayList<SyncItemInfo>();
+            ArrayList<SyncItemInfo> syncItemInfos = new ArrayList<>();
             for (DbSyncItemInfo dbSyncItemInfo : userTrackingService.getDbSyncItemInfos(startUuid)) {
                 syncItemInfos.add(dbSyncItemInfo.getSyncItemInfo());
             }
             playbackInfo.setSyncItemInfos(syncItemInfos);
 
             // Scrolling
-            ArrayList<TerrainScrollTracking> terrainScrollTrackings = new ArrayList<TerrainScrollTracking>();
+            ArrayList<TerrainScrollTracking> terrainScrollTrackings = new ArrayList<>();
             for (DbScrollTrackingItem dbScrollTrackingItem : userTrackingService.getDbScrollTrackingItems(startUuid)) {
                 terrainScrollTrackings.add(dbScrollTrackingItem.createScrollTrackingItem());
             }
             playbackInfo.setScrollTrackingItems(terrainScrollTrackings);
 
             // Browser window tracking
-            ArrayList<BrowserWindowTracking> browserWindowTrackings = new ArrayList<BrowserWindowTracking>();
+            ArrayList<BrowserWindowTracking> browserWindowTrackings = new ArrayList<>();
             for (DbBrowserWindowTracking dbBrowserWindowTracking : userTrackingService.getDbBrowserWindowTrackings(startUuid)) {
                 browserWindowTrackings.add(dbBrowserWindowTracking.createBrowserWindowTracking());
             }
             playbackInfo.setBrowserWindowTrackings(browserWindowTrackings);
 
             // Dialogs
-            ArrayList<DialogTracking> dialogTrackings = new ArrayList<DialogTracking>();
+            ArrayList<DialogTracking> dialogTrackings = new ArrayList<>();
             for (DbDialogTracking dbDialogTracking : userTrackingService.getDbDialogTrackings(startUuid)) {
                 dialogTrackings.add(dbDialogTracking.createDialogTracking());
             }
@@ -114,6 +131,6 @@ public class PlaybackServiceImpl extends AutowiredRemoteServiceServlet implement
         } catch (Throwable t) {
             log.error("", t);
             return null;
-        } */
+        }
     }
 }
