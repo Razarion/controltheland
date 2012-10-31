0c1d084bcbfdfc2bea1b63f79eef382eb16fa58d
beat.keller@btxtech.com <beat.keller@btxtech.com@4948f9da-d142-11de-aacf-132551bdafba>
2012-10-31 16:38:21 +0000

    Graphic cosmetics

--- trunk/controltheland/src/main/java/com/btxtech/game/jsre/client/dialogs/inventory/BuyPanel.java
+++ trunk/controltheland/src/main/java/com/btxtech/game/jsre/client/dialogs/inventory/BuyPanel.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.btxtech.game.jsre.client.dialogs.inventory;
 
@@ -7,28 +7,20 @@ import com.btxtech.game.jsre.client.Connection;
 import com.btxtech.game.jsre.client.dialogs.DialogManager;
 import com.btxtech.game.jsre.client.dialogs.RegisterDialog;
 import com.btxtech.game.jsre.common.PayPalUtils;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.dom.client.Element;
-import com.google.gwt.dom.client.SpanElement;
+import com.google.gwt.dom.client.Style;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.UIObject;
 import com.google.gwt.user.client.ui.VerticalPanel;
-import com.google.gwt.user.client.ui.Widget;
 
 /**
  * @author beat
- * 
  */
 public class BuyPanel extends VerticalPanel {
     public BuyPanel(InventoryDialog inventoryDialog) {
-        add(new HTML("<h2>Buy Razarion via PayPal</h2>"));
+        add(createHtml("<h2>Buy Razarion via PayPal</h2>"));
 
         if (Connection.getInstance().isRegistered()) {
             fillBuyOptions();
@@ -37,20 +29,28 @@ public class BuyPanel extends VerticalPanel {
         }
     }
 
+    private HTML createHtml(String htmlString) {
+        HTML html = new HTML(htmlString);
+        html.getElement().getStyle().setColor("#C7C4BB");
+        html.getElement().getStyle().setFontSize(14, Style.Unit.PX);
+        html.getElement().getStyle().setProperty("fontFamily", "Arial, Helvetica, sans-serif");
+        return html;
+    }
+
     private void fillBuyOptions() {
         FlexTable flexTable = new FlexTable();
         // 1000 Razarion
         flexTable.setWidget(0, 0, PayPalUtils.createBuyNowButton("PE44JA3J2AZ2J"));
-        flexTable.setWidget(0, 1, new HTML("1000 Razarion for 5$"));
+        flexTable.setWidget(0, 1, createHtml("1000 Razarion for 5$"));
         // 2200 Razarion
         flexTable.setWidget(1, 0, PayPalUtils.createBuyNowButton("T6UDKDH59Y43E"));
-        flexTable.setWidget(1, 1, new HTML("2200 Razarion for 10$.<br />You get 200 Razarion for free!"));
+        flexTable.setWidget(1, 1, createHtml("2200 Razarion for 10$.<br />You get 200 Razarion for free!"));
         // 4600 Razarion
         flexTable.setWidget(2, 0, PayPalUtils.createBuyNowButton("7LSHFG9LM88VL"));
-        flexTable.setWidget(2, 1, new HTML("4600 Razarion for 20$.<br />You get 600 Razarion for free!"));
+        flexTable.setWidget(2, 1, createHtml("4600 Razarion for 20$.<br />You get 600 Razarion for free!"));
         // 12500 Razarion
         flexTable.setWidget(3, 0, PayPalUtils.createBuyNowButton("YLVYNLXBSJXGY"));
-        flexTable.setWidget(3, 1, new HTML("12500 Razarion for 50$.<br />You get 2500 Razarion for free!"));
+        flexTable.setWidget(3, 1, createHtml("12500 Razarion for 50$.<br />You get 2500 Razarion for free!"));
         add(flexTable);
     }
 
