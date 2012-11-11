88b8735c8ab0e26c4c77313145052e0b8f9461f8
beat.keller@btxtech.com <beat.keller@btxtech.com@4948f9da-d142-11de-aacf-132551bdafba>
2012-11-11 13:08:13 +0000

    Test fixed

--- trunk/controltheland/src/test/java/com/btxtech/game/jsre/common/gameengine/syncObjects/TestItemTypeSpriteMap.java
+++ trunk/controltheland/src/test/java/com/btxtech/game/jsre/common/gameengine/syncObjects/TestItemTypeSpriteMap.java
@@ -9,20 +9,19 @@ import org.easymock.EasyMock;
 import org.junit.Test;
 
 /**
- * User: beat
- * Date: 02.08.12
- * Time: 22:27
+ * User: beat Date: 02.08.12 Time: 22:27
  */
 public class TestItemTypeSpriteMap {
     @Test
     public void testRuntime() {
-        BoundingBox boundingBox = new BoundingBox(80, new double[]{0.0, 0.1, 0.2, 0.3, 0.4});
+        BoundingBox boundingBox = new BoundingBox(80, new double[] { 0.0, 0.1, 0.2, 0.3, 0.4 });
         ItemTypeSpriteMap itemTypeSpriteMap = new ItemTypeSpriteMap(boundingBox, 100, 200, 4, 3, 25, 4, 30, null);
 
         SyncItemArea syncItemArea = EasyMock.createNiceMock(SyncItemArea.class);
         EasyMock.expect(syncItemArea.getAngel()).andReturn(0.0).anyTimes();
         SyncBaseItem syncBaseItemMock = EasyMock.createNiceMock(SyncBaseItem.class);
         EasyMock.expect(syncBaseItemMock.isReady()).andReturn(true).anyTimes();
+        EasyMock.expect(syncBaseItemMock.isAlive()).andReturn(true).anyTimes();
         EasyMock.expect(syncBaseItemMock.isHealthy()).andReturn(true).anyTimes();
         EasyMock.expect(syncBaseItemMock.getSyncItemArea()).andReturn(syncItemArea).anyTimes();
         EasyMock.replay(syncItemArea, syncBaseItemMock);
@@ -41,13 +40,15 @@ public class TestItemTypeSpriteMap {
 
     @Test
     public void testDemolitionStep1() {
-        BoundingBox boundingBox = new BoundingBox(80, new double[]{0.0, 0.1, 0.2, 0.3});
-        ItemTypeSpriteMap itemTypeSpriteMap = new ItemTypeSpriteMap(boundingBox, 100, 200, 4, 3, 25, 5, 30, new DemolitionStepSpriteMap[]{new DemolitionStepSpriteMap(1, 100, null), new DemolitionStepSpriteMap(3, 200, null)});
+        BoundingBox boundingBox = new BoundingBox(80, new double[] { 0.0, 0.1, 0.2, 0.3 });
+        ItemTypeSpriteMap itemTypeSpriteMap = new ItemTypeSpriteMap(boundingBox, 100, 200, 4, 3, 25, 5, 30, new DemolitionStepSpriteMap[] {
+                new DemolitionStepSpriteMap(1, 100, null), new DemolitionStepSpriteMap(3, 200, null) });
 
         SyncItemArea syncItemArea = EasyMock.createNiceMock(SyncItemArea.class);
         EasyMock.expect(syncItemArea.getAngel()).andReturn(0.0).anyTimes();
         SyncBaseItem syncBaseItemMock = EasyMock.createNiceMock(SyncBaseItem.class);
         EasyMock.expect(syncBaseItemMock.isReady()).andReturn(true).anyTimes();
+        EasyMock.expect(syncBaseItemMock.isAlive()).andReturn(true).anyTimes();
         EasyMock.expect(syncBaseItemMock.isHealthy()).andReturn(false).anyTimes();
         EasyMock.expect(syncBaseItemMock.getNormalizedHealth()).andReturn(0.75).anyTimes();
         EasyMock.expect(syncBaseItemMock.getSyncItemArea()).andReturn(syncItemArea).anyTimes();
@@ -61,13 +62,15 @@ public class TestItemTypeSpriteMap {
 
     @Test
     public void testDemolitionStep2() {
-        BoundingBox boundingBox = new BoundingBox(80, new double[]{0.0, 0.1, 0.2, 0.3});
-        ItemTypeSpriteMap itemTypeSpriteMap = new ItemTypeSpriteMap(boundingBox, 100, 200, 4, 3, 25, 5, 30, new DemolitionStepSpriteMap[]{new DemolitionStepSpriteMap(1, 100, null), new DemolitionStepSpriteMap(3, 200, null)});
+        BoundingBox boundingBox = new BoundingBox(80, new double[] { 0.0, 0.1, 0.2, 0.3 });
+        ItemTypeSpriteMap itemTypeSpriteMap = new ItemTypeSpriteMap(boundingBox, 100, 200, 4, 3, 25, 5, 30, new DemolitionStepSpriteMap[] {
+                new DemolitionStepSpriteMap(1, 100, null), new DemolitionStepSpriteMap(3, 200, null) });
 
         SyncItemArea syncItemArea = EasyMock.createNiceMock(SyncItemArea.class);
         EasyMock.expect(syncItemArea.getAngel()).andReturn(0.0).anyTimes();
         SyncBaseItem syncBaseItemMock = EasyMock.createNiceMock(SyncBaseItem.class);
         EasyMock.expect(syncBaseItemMock.isReady()).andReturn(true).anyTimes();
+        EasyMock.expect(syncBaseItemMock.isAlive()).andReturn(true).anyTimes();
         EasyMock.expect(syncBaseItemMock.isHealthy()).andReturn(false).anyTimes();
         EasyMock.expect(syncBaseItemMock.getNormalizedHealth()).andReturn(0.25).anyTimes();
         EasyMock.expect(syncBaseItemMock.getSyncItemArea()).andReturn(syncItemArea).anyTimes();
@@ -81,13 +84,15 @@ public class TestItemTypeSpriteMap {
 
     @Test
     public void testDemolitionStep2SecondAngel() {
-        BoundingBox boundingBox = new BoundingBox(80, new double[]{0.0, 0.1, 0.2, 0.3});
-        ItemTypeSpriteMap itemTypeSpriteMap = new ItemTypeSpriteMap(boundingBox, 100, 200, 4, 3, 25, 5, 30, new DemolitionStepSpriteMap[]{new DemolitionStepSpriteMap(1, 100, null), new DemolitionStepSpriteMap(3, 200, null)});
+        BoundingBox boundingBox = new BoundingBox(80, new double[] { 0.0, 0.1, 0.2, 0.3 });
+        ItemTypeSpriteMap itemTypeSpriteMap = new ItemTypeSpriteMap(boundingBox, 100, 200, 4, 3, 25, 5, 30, new DemolitionStepSpriteMap[] {
+                new DemolitionStepSpriteMap(1, 100, null), new DemolitionStepSpriteMap(3, 200, null) });
 
         SyncItemArea syncItemArea = EasyMock.createNiceMock(SyncItemArea.class);
         EasyMock.expect(syncItemArea.getAngel()).andReturn(0.1).anyTimes();
         SyncBaseItem syncBaseItemMock = EasyMock.createNiceMock(SyncBaseItem.class);
         EasyMock.expect(syncBaseItemMock.isReady()).andReturn(true).anyTimes();
+        EasyMock.expect(syncBaseItemMock.isAlive()).andReturn(true).anyTimes();
         EasyMock.expect(syncBaseItemMock.isHealthy()).andReturn(false).anyTimes();
         EasyMock.expect(syncBaseItemMock.getNormalizedHealth()).andReturn(0.25).anyTimes();
         EasyMock.expect(syncBaseItemMock.getSyncItemArea()).andReturn(syncItemArea).anyTimes();
@@ -99,16 +104,17 @@ public class TestItemTypeSpriteMap {
         Assert.assertEquals(new Index(3700, 0), itemTypeSpriteMap.getItemTypeImageOffset(syncBaseItemMock, 600));
     }
 
-
     @Test
     public void testDemolitionStep1NoImages() {
-        BoundingBox boundingBox = new BoundingBox(80, new double[]{0.0, 0.1, 0.2, 0.3, 0.4});
-        ItemTypeSpriteMap itemTypeSpriteMap = new ItemTypeSpriteMap(boundingBox, 100, 200, 4, 3, 25, 4, 30, new DemolitionStepSpriteMap[]{new DemolitionStepSpriteMap(0, 0, null), new DemolitionStepSpriteMap(0, 0, null)});
+        BoundingBox boundingBox = new BoundingBox(80, new double[] { 0.0, 0.1, 0.2, 0.3, 0.4 });
+        ItemTypeSpriteMap itemTypeSpriteMap = new ItemTypeSpriteMap(boundingBox, 100, 200, 4, 3, 25, 4, 30, new DemolitionStepSpriteMap[] {
+                new DemolitionStepSpriteMap(0, 0, null), new DemolitionStepSpriteMap(0, 0, null) });
 
         SyncItemArea syncItemArea = EasyMock.createNiceMock(SyncItemArea.class);
         EasyMock.expect(syncItemArea.getAngel()).andReturn(0.0).anyTimes();
         SyncBaseItem syncBaseItemMock = EasyMock.createNiceMock(SyncBaseItem.class);
         EasyMock.expect(syncBaseItemMock.isReady()).andReturn(true).anyTimes();
+        EasyMock.expect(syncBaseItemMock.isAlive()).andReturn(true).anyTimes();
         EasyMock.expect(syncBaseItemMock.isHealthy()).andReturn(false).anyTimes();
         EasyMock.expect(syncBaseItemMock.getNormalizedHealth()).andReturn(0.25).anyTimes();
         EasyMock.expect(syncBaseItemMock.getSyncItemArea()).andReturn(syncItemArea).anyTimes();
@@ -126,16 +132,17 @@ public class TestItemTypeSpriteMap {
         Assert.assertEquals(new Index(1500, 0), itemTypeSpriteMap.getItemTypeImageOffset(syncBaseItemMock, 215));
     }
 
-
     @Test
     public void testDemolitionStep2NoImages() {
-        BoundingBox boundingBox = new BoundingBox(80, new double[]{0.0, 0.1, 0.2, 0.3});
-        ItemTypeSpriteMap itemTypeSpriteMap = new ItemTypeSpriteMap(boundingBox, 100, 200, 4, 3, 25, 5, 30, new DemolitionStepSpriteMap[]{new DemolitionStepSpriteMap(1, 100, null), new DemolitionStepSpriteMap(0, 0, null)});
+        BoundingBox boundingBox = new BoundingBox(80, new double[] { 0.0, 0.1, 0.2, 0.3 });
+        ItemTypeSpriteMap itemTypeSpriteMap = new ItemTypeSpriteMap(boundingBox, 100, 200, 4, 3, 25, 5, 30, new DemolitionStepSpriteMap[] {
+                new DemolitionStepSpriteMap(1, 100, null), new DemolitionStepSpriteMap(0, 0, null) });
 
         SyncItemArea syncItemArea = EasyMock.createNiceMock(SyncItemArea.class);
         EasyMock.expect(syncItemArea.getAngel()).andReturn(0.0).anyTimes();
         SyncBaseItem syncBaseItemMock = EasyMock.createNiceMock(SyncBaseItem.class);
         EasyMock.expect(syncBaseItemMock.isReady()).andReturn(true).anyTimes();
+        EasyMock.expect(syncBaseItemMock.isAlive()).andReturn(true).anyTimes();
         EasyMock.expect(syncBaseItemMock.isHealthy()).andReturn(false).anyTimes();
         EasyMock.expect(syncBaseItemMock.getNormalizedHealth()).andReturn(0.25).anyTimes();
         EasyMock.expect(syncBaseItemMock.getSyncItemArea()).andReturn(syncItemArea).anyTimes();
@@ -145,6 +152,54 @@ public class TestItemTypeSpriteMap {
         Assert.assertEquals(new Index(3200, 0), itemTypeSpriteMap.getItemTypeImageOffset(syncBaseItemMock, 100));
         Assert.assertEquals(new Index(3200, 0), itemTypeSpriteMap.getItemTypeImageOffset(syncBaseItemMock, 200));
         Assert.assertEquals(new Index(3200, 0), itemTypeSpriteMap.getItemTypeImageOffset(syncBaseItemMock, 300));
+    }
+    
+    @Test
+    public void testDemolitionStepIsDead() {
+        BoundingBox boundingBox = new BoundingBox(80, new double[] { 0.0, 0.1, 0.2, 0.3 });
+        ItemTypeSpriteMap itemTypeSpriteMap = new ItemTypeSpriteMap(boundingBox, 100, 200, 4, 3, 25, 5, 30, new DemolitionStepSpriteMap[] {
+                new DemolitionStepSpriteMap(1, 100, null), new DemolitionStepSpriteMap(3, 200, null) });
+
+        SyncItemArea syncItemArea = EasyMock.createNiceMock(SyncItemArea.class);
+        EasyMock.expect(syncItemArea.getAngel()).andReturn(0.0).anyTimes();
+        SyncBaseItem syncBaseItemMock = EasyMock.createNiceMock(SyncBaseItem.class);
+        EasyMock.expect(syncBaseItemMock.isReady()).andReturn(true).anyTimes();
+        EasyMock.expect(syncBaseItemMock.isAlive()).andReturn(false).anyTimes();
+        EasyMock.expect(syncBaseItemMock.getNormalizedHealth()).andReturn(0.75).anyTimes();
+        EasyMock.expect(syncBaseItemMock.getSyncItemArea()).andReturn(syncItemArea).anyTimes();
+        EasyMock.replay(syncItemArea, syncBaseItemMock);
+
+        Assert.assertEquals(new Index(3300, 0), itemTypeSpriteMap.getItemTypeImageOffset(syncBaseItemMock, 0));
+        Assert.assertEquals(new Index(3400, 0), itemTypeSpriteMap.getItemTypeImageOffset(syncBaseItemMock, 200));
+        Assert.assertEquals(new Index(3500, 0), itemTypeSpriteMap.getItemTypeImageOffset(syncBaseItemMock, 400));
+        Assert.assertEquals(new Index(3300, 0), itemTypeSpriteMap.getItemTypeImageOffset(syncBaseItemMock, 600));
+    }
+    
+    @Test
+    public void testDemolitionStepNoImagesIsDead() {
+        BoundingBox boundingBox = new BoundingBox(80, new double[] { 0.0, 0.1, 0.2, 0.3, 0.4 });
+        ItemTypeSpriteMap itemTypeSpriteMap = new ItemTypeSpriteMap(boundingBox, 100, 200, 4, 3, 25, 4, 30, new DemolitionStepSpriteMap[] {
+                new DemolitionStepSpriteMap(0, 0, null), new DemolitionStepSpriteMap(0, 0, null) });
+
+        SyncItemArea syncItemArea = EasyMock.createNiceMock(SyncItemArea.class);
+        EasyMock.expect(syncItemArea.getAngel()).andReturn(0.0).anyTimes();
+        SyncBaseItem syncBaseItemMock = EasyMock.createNiceMock(SyncBaseItem.class);
+        EasyMock.expect(syncBaseItemMock.isReady()).andReturn(true).anyTimes();
+        EasyMock.expect(syncBaseItemMock.isAlive()).andReturn(false).anyTimes();
+        EasyMock.expect(syncBaseItemMock.getNormalizedHealth()).andReturn(0.25).anyTimes();
+        EasyMock.expect(syncBaseItemMock.getSyncItemArea()).andReturn(syncItemArea).anyTimes();
+        EasyMock.replay(syncItemArea, syncBaseItemMock);
+
+        Assert.assertEquals(new Index(1200, 0), itemTypeSpriteMap.getItemTypeImageOffset(syncBaseItemMock, 0));
+        Assert.assertEquals(new Index(1200, 0), itemTypeSpriteMap.getItemTypeImageOffset(syncBaseItemMock, 29));
+        Assert.assertEquals(new Index(1300, 0), itemTypeSpriteMap.getItemTypeImageOffset(syncBaseItemMock, 30));
+        Assert.assertEquals(new Index(1300, 0), itemTypeSpriteMap.getItemTypeImageOffset(syncBaseItemMock, 31));
+        Assert.assertEquals(new Index(1400, 0), itemTypeSpriteMap.getItemTypeImageOffset(syncBaseItemMock, 60));
+        Assert.assertEquals(new Index(1500, 0), itemTypeSpriteMap.getItemTypeImageOffset(syncBaseItemMock, 90));
+        Assert.assertEquals(new Index(1200, 0), itemTypeSpriteMap.getItemTypeImageOffset(syncBaseItemMock, 120));
+        Assert.assertEquals(new Index(1300, 0), itemTypeSpriteMap.getItemTypeImageOffset(syncBaseItemMock, 155));
+        Assert.assertEquals(new Index(1400, 0), itemTypeSpriteMap.getItemTypeImageOffset(syncBaseItemMock, 185));
+        Assert.assertEquals(new Index(1500, 0), itemTypeSpriteMap.getItemTypeImageOffset(syncBaseItemMock, 215));
     }
 
 }
