1a0337d8ecd8c921ac503291b6424c0d300d9f36
beat.keller@btxtech.com <beat.keller@btxtech.com@4948f9da-d142-11de-aacf-132551bdafba>
2012-11-26 00:02:08 +0000

    Bugfix

--- trunk/controltheland/src/main/java/com/btxtech/game/services/gwt/ExtendedRemoteLoggingServiceImpl.java
+++ trunk/controltheland/src/main/java/com/btxtech/game/services/gwt/ExtendedRemoteLoggingServiceImpl.java
@@ -7,6 +7,9 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -72,4 +75,14 @@ public class ExtendedRemoteLoggingServiceImpl extends RemoteServiceServlet imple
         }
     }
 
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        WebApplicationContext ctx = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
+        if (ctx == null) {
+            throw new IllegalStateException("No Spring web application");
+        }
+        // wire the bean
+        ctx.getAutowireCapableBeanFactory().autowireBeanProperties(this, AutowireCapableBeanFactory.AUTOWIRE_BY_TYPE, true);
+    }
 }
