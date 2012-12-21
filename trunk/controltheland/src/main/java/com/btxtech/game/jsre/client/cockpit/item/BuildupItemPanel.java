593ec173a737e4f804e5bccebdcfec23d581f7ae
beat.keller@btxtech.com <beat.keller@btxtech.com@4948f9da-d142-11de-aacf-132551bdafba>
2012-12-21 13:48:42 +0000

    ItemCockpit: changes to due level and allowed item improved

--- trunk/controltheland/src/main/java/com/btxtech/game/jsre/client/cockpit/item/BuildupItemPanel.java
+++ trunk/controltheland/src/main/java/com/btxtech/game/jsre/client/cockpit/item/BuildupItemPanel.java
@@ -13,10 +13,6 @@
 
 package com.btxtech.game.jsre.client.cockpit.item;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-
 import com.btxtech.game.jsre.client.ClientExceptionHandler;
 import com.btxtech.game.jsre.client.ClientGlobalServices;
 import com.btxtech.game.jsre.client.ClientPlanetServices;
@@ -39,6 +35,10 @@ import com.google.gwt.user.client.ui.PushButton;
 import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.Widget;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * User: beat Date: 15.11.2009 Time: 14:12:18
  */
@@ -53,6 +53,7 @@ public class BuildupItemPanel extends Composite {
     ScrollPanel scrollPanel;
 
     private Map<Integer, BuildupItem> buildupItems = new HashMap<Integer, BuildupItem>();
+    private Group selectedGroup;
 
     interface BuildupItemPanelUiBinder extends UiBinder<Widget, BuildupItemPanel> {
     }
@@ -73,22 +74,26 @@ public class BuildupItemPanel extends Composite {
     }
 
     public void display(SyncBaseItem syncBaseItem) {
+        selectedGroup = null;
         try {
             if (syncBaseItem.hasSyncBuilder()) {
                 Group group = new Group();
                 group.addItem(syncBaseItem);
+                selectedGroup = group;
                 setupBuildupItemsCV(group);
             } else if (syncBaseItem.hasSyncFactory()) {
                 Group group = new Group();
                 group.addItem(syncBaseItem);
+                selectedGroup = group;
                 setupBuildupItemsFactory(group);
             }
         } catch (NoSuchItemTypeException e) {
-            ClientExceptionHandler.handleException(e);
+            ClientExceptionHandler.handleException("BuildupItemPanel.display(syncBaseItem) " + syncBaseItem, e);
         }
     }
 
     public void display(Group selectedGroup) {
+        this.selectedGroup = selectedGroup;
         try {
             if (selectedGroup.onlyConstructionVehicle()) {
                 setupBuildupItemsCV(selectedGroup);
@@ -96,7 +101,7 @@ public class BuildupItemPanel extends Composite {
                 setupBuildupItemsFactory(selectedGroup);
             }
         } catch (NoSuchItemTypeException e) {
-            ClientExceptionHandler.handleException(e);
+            ClientExceptionHandler.handleException("BuildupItemPanel.display(selectedGroup) " + selectedGroup, e);
         }
     }
 
@@ -105,7 +110,7 @@ public class BuildupItemPanel extends Composite {
         HorizontalPanel itemsToBuild = new HorizontalPanel();
         Collection<Integer> itemTypeIDs = constructionVehicles.getFirst().getBaseItemType().getBuilderType().getAbleToBuild();
         for (Integer itemTypeID : itemTypeIDs) {
-            if(ClientPlanetServices.getInstance().getPlanetInfo().getLimitation4ItemType(itemTypeID) == 0) {
+            if (ClientPlanetServices.getInstance().getPlanetInfo().getLimitation4ItemType(itemTypeID) == 0) {
                 continue;
             }
             final BaseItemType itemType = (BaseItemType) ClientGlobalServices.getInstance().getItemTypeService().getItemType(itemTypeID);
@@ -125,7 +130,7 @@ public class BuildupItemPanel extends Composite {
         HorizontalPanel itemsToBuild = new HorizontalPanel();
         Collection<Integer> itemTypeIDs = factories.getFirst().getBaseItemType().getFactoryType().getAbleToBuild();
         for (Integer itemTypeID : itemTypeIDs) {
-            if(ClientPlanetServices.getInstance().getPlanetInfo().getLimitation4ItemType(itemTypeID) == 0) {
+            if (ClientPlanetServices.getInstance().getPlanetInfo().getLimitation4ItemType(itemTypeID) == 0) {
                 continue;
             }
             final BaseItemType itemType = (BaseItemType) ClientGlobalServices.getInstance().getItemTypeService().getItemType(itemTypeID);
@@ -157,11 +162,8 @@ public class BuildupItemPanel extends Composite {
     }
 
     public void onStateChanged() {
-        for (BuildupItem buildupItem : this.buildupItems.values()) {
-            buildupItem.onStateChanged();
-        }
+        display(selectedGroup);
     }
-
 
     public Index getAbsoluteMiddleTopPosition(int buildupItemTypeId) {
         BuildupItem buildupItem = buildupItems.get(buildupItemTypeId);
