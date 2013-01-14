55b84d4ff0efa5dfc3e80bd6e807e416f8343a95
beat.keller <beat.keller@btxtech.com>
2013-01-14 16:54:32 +0000

    Quest dialog improved

--- controltheland/src/main/java/com/btxtech/game/jsre/client/dialogs/quest/QuestPlate.java
+++ controltheland/src/main/java/com/btxtech/game/jsre/client/dialogs/quest/QuestPlate.java
@@ -17,9 +17,12 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.Image;
 
 public class QuestPlate extends Composite {
     private static QuestPlateUiBinder uiBinder = GWT.create(QuestPlateUiBinder.class);
+    private static QuestImages questImages = GWT.create(QuestImages.class);
     private int questId;
     private QuestDialog questDialog;
     @UiField
@@ -30,16 +33,32 @@ public class QuestPlate extends Composite {
     Label rewards;
     @UiField
     HTML description;
+    @UiField Label typeLabel;
+    @UiField Label typeTitleLabel;
+    @UiField(provided = true) Image icon;
 
     interface QuestPlateUiBinder extends UiBinder<Widget, QuestPlate> {
     }
 
     public QuestPlate(QuestInfo info, QuestDialog questDialog) {
         this.questDialog = questDialog;
+        if(info.getType() == QuestInfo.Type.MISSION) {
+            icon = new Image(questImages.mission()); 
+        } else {
+            icon = new Image(questImages.quest()); 
+        }
         initWidget(uiBinder.createAndBindUi(this));
         questId = info.getId();
         titel.setText(info.getTitle());
         rewards.setText(ClientI18nHelper.CONSTANTS.reward(info.getGold(), info.getXp()));
+        if(QuestTypeEnum.isVisible(info.getQuestTypeEnum())) {
+            typeTitleLabel.setVisible(true); 
+            typeLabel.setVisible(true);
+            typeLabel.setText(info.getQuestTypeEnum().getString());
+        } else {
+            typeTitleLabel.setVisible(false); 
+            typeLabel.setVisible(false); 
+        }
         description.setHTML(info.getDescription());
     }
 
