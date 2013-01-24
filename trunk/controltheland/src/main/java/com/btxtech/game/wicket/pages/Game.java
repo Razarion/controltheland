6ca9251adeb7115a3b5e9368057a938e39a04116
beat.keller@btxtech.com <beat.keller@btxtech.com@4948f9da-d142-11de-aacf-132551bdafba>
2013-01-24 22:48:17 +0000

    Cosmetics

--- trunk/controltheland/src/main/java/com/btxtech/game/wicket/pages/Game.java
+++ trunk/controltheland/src/main/java/com/btxtech/game/wicket/pages/Game.java
@@ -26,7 +26,6 @@ import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.ResourceModel;
-import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 /**
