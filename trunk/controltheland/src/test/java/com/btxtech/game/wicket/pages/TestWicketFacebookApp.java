cba337cab22129bbdb534ff14456d47cf2daf629
beat.keller@btxtech.com <beat.keller@btxtech.com@4948f9da-d142-11de-aacf-132551bdafba>
2013-08-30 11:33:38 +0000

    Facebook login bugfixes

--- trunk/controltheland/src/test/java/com/btxtech/game/wicket/pages/TestWicketFacebookApp.java
+++ trunk/controltheland/src/test/java/com/btxtech/game/wicket/pages/TestWicketFacebookApp.java
@@ -7,7 +7,6 @@ import com.btxtech.game.wicket.uiservices.cms.CmsUiService;
 import com.btxtech.game.wicket.uiservices.cms.impl.CmsUiServiceImpl;
 import org.apache.wicket.Page;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.util.tester.FormTester;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,23 +21,12 @@ import java.util.Locale;
  * Time: 09:53
  */
 public class TestWicketFacebookApp extends AbstractServiceTest {
-    public static final String FACEBOOK_OAUTH_REDIRECT = "<script type=\"text/javascript\">\n" +
-            "        /*<![CDATA[*/\n" +
-            "        var oauth_url = 'https://www.facebook.com/dialog/oauth/';\n" +
-            "        oauth_url += '?client_id=195335357236873';\n" +
-            "        oauth_url += '&redirect_uri=' + encodeURIComponent('https://apps.facebook.com/razarion/');\n" +
-            "        oauth_url += '&scope=' + encodeURIComponent('email');\n" +
-            "        window.top.location = oauth_url;\n" +
-            "        /*]]>*/\n" +
-            "    </script>";
     @Autowired
     private CmsUiService cmsUiService;
 
     @Test
     @DirtiesContext
     public void newUserRegisterEnterGame() throws Exception {
-        Assert.fail();
-        /*
         configureMultiplePlanetsAndLevels();
 
         // Do not rejoice too quickly Just... this is just a test secret.
@@ -49,36 +37,17 @@ public class TestWicketFacebookApp extends AbstractServiceTest {
         // First authorization -> OAuth dialog
         getWicketTester().getRequest().getPostParameters().setParameterValue("signed_request", "3RaYyXwkwhCc4OlVfDhAU9Y-O_pyqN4mgE7JyzPwcIc.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImlzc3VlZF9hdCI6MTM0MzE0NjYzOSwidXNlciI6eyJjb3VudHJ5IjoiY2giLCJsb2NhbGUiOiJlbl9VUyIsImFnZSI6eyJtaW4iOjIxfX19");
         getWicketTester().startPage(FacebookAppStart.class);
-        assertStringInHeader(FACEBOOK_OAUTH_REDIRECT);
-        endHttpRequestAndOpenSessionInViewFilter();
-        // User accepted & choose nick name
-        beginHttpRequestAndOpenSessionInViewFilter();
-        getWicketTester().getRequest().getPostParameters().setParameterValue("signed_request", "v3-O8s1WrS9B2XnYXpRo61n2hKc9wboofRDHOxcF8XI.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImV4cGlyZXMiOjEzNDMxNTI4MDAsImlzc3VlZF9hdCI6MTM0MzE0NjY4Mywib2F1dGhfdG9rZW4iOiJBQUFFa3RlWVZ1WkNNQkFDS29mOGpkWDMxcnVTWkN3RXFuRnFWd3Z2NnBBNldNMTVaQ1V6bzlRNmliUXJiWGtRVkJOeEF0UDJmc2EzVzY3ZXJITW5EWkFvNlZHRzVPajg4U2FJMWZOYkVyYjhCeDBuOURRWkIyIiwidXNlciI6eyJjb3VudHJ5IjoiY2giLCJsb2NhbGUiOiJlbl9VUyIsImFnZSI6eyJtaW4iOjIxfX0sInVzZXJfaWQiOiIxMDAwMDM2MzQwOTQxMzkifQ");
-        getWicketTester().getRequest().getPostParameters().setParameterValue("email", "fakeEmail");
-        getWicketTester().startPage(FacebookAppStart.class);
-        getWicketTester().assertNoErrorMessage();
-        getWicketTester().assertRenderedPage(FacebookAppNickName.class);
-        assertStringNotInHeader("https://www.facebook.com/dialog/oauth/");
-        // Enter valid nickname
-        FormTester formTester = getWicketTester().newFormTester("form");
-        formTester.setValue("name", "xxx");
-        formTester.submit("goButton");
         getWicketTester().assertRenderedPage(Game.class);
         Page gamePage = getWicketTester().getLastRenderedPage();
         Assert.assertEquals(TEST_LEVEL_TASK_1_1_SIMULATED_ID, gamePage.getPageParameters().get("taskId").toInt());
-        User user = getUser();
-        Assert.assertEquals(User.SocialNet.FACEBOOK, user.getSocialNet());
-        Assert.assertEquals("xxx", user.getUsername());
-        Assert.assertEquals("100003634094139", user.getSocialNetUserId());
-        Assert.assertEquals("fakeEmail", user.getEmail());
+        Assert.assertNull(getUser());
         endHttpRequestAndOpenSessionInViewFilter();
         endHttpSession();
-        */
     }
 
     @Test
     @DirtiesContext
-    public void registeredUserEnterGame() throws Exception {
+    public void registeredByFacebookUnregisteredByGame() throws Exception {
         configureMultiplePlanetsAndLevels();
         // Do not rejoice too quickly... this is just a test secret.
         setPrivateField(CmsUiServiceImpl.class, cmsUiService, "facebookAppSecret", "029a30fb9677d35c79c44d8a505d8fe1");
@@ -94,7 +63,6 @@ public class TestWicketFacebookApp extends AbstractServiceTest {
         getWicketTester().getRequest().getPostParameters().setParameterValue("signed_request", "v3-O8s1WrS9B2XnYXpRo61n2hKc9wboofRDHOxcF8XI.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImV4cGlyZXMiOjEzNDMxNTI4MDAsImlzc3VlZF9hdCI6MTM0MzE0NjY4Mywib2F1dGhfdG9rZW4iOiJBQUFFa3RlWVZ1WkNNQkFDS29mOGpkWDMxcnVTWkN3RXFuRnFWd3Z2NnBBNldNMTVaQ1V6bzlRNmliUXJiWGtRVkJOeEF0UDJmc2EzVzY3ZXJITW5EWkFvNlZHRzVPajg4U2FJMWZOYkVyYjhCeDBuOURRWkIyIiwidXNlciI6eyJjb3VudHJ5IjoiY2giLCJsb2NhbGUiOiJlbl9VUyIsImFnZSI6eyJtaW4iOjIxfX0sInVzZXJfaWQiOiIxMDAwMDM2MzQwOTQxMzkifQ");
         getWicketTester().startPage(FacebookAppStart.class);
         getWicketTester().assertNoErrorMessage();
-        assertStringNotInHeader(FACEBOOK_OAUTH_REDIRECT);
         getWicketTester().assertRenderedPage(Game.class);
         Page gamePage = getWicketTester().getLastRenderedPage();
         Assert.assertEquals(TEST_LEVEL_TASK_1_1_SIMULATED_ID, gamePage.getPageParameters().get("taskId").toInt());
@@ -108,9 +76,7 @@ public class TestWicketFacebookApp extends AbstractServiceTest {
     @Test
     @DirtiesContext
     public void unregisteredUserEnterGame() throws Exception {
-        Assert.fail();
-        /*
-      configureMultiplePlanetsAndLevels();
+        configureMultiplePlanetsAndLevels();
         // Do not rejoice too quickly... this is just a test secret.
         setPrivateField(CmsUiServiceImpl.class, cmsUiService, "facebookAppSecret", "029a30fb9677d35c79c44d8a505d8fe1");
         // Login
@@ -119,24 +85,10 @@ public class TestWicketFacebookApp extends AbstractServiceTest {
         getWicketTester().getRequest().getPostParameters().setParameterValue("signed_request", "v3-O8s1WrS9B2XnYXpRo61n2hKc9wboofRDHOxcF8XI.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImV4cGlyZXMiOjEzNDMxNTI4MDAsImlzc3VlZF9hdCI6MTM0MzE0NjY4Mywib2F1dGhfdG9rZW4iOiJBQUFFa3RlWVZ1WkNNQkFDS29mOGpkWDMxcnVTWkN3RXFuRnFWd3Z2NnBBNldNMTVaQ1V6bzlRNmliUXJiWGtRVkJOeEF0UDJmc2EzVzY3ZXJITW5EWkFvNlZHRzVPajg4U2FJMWZOYkVyYjhCeDBuOURRWkIyIiwidXNlciI6eyJjb3VudHJ5IjoiY2giLCJsb2NhbGUiOiJlbl9VUyIsImFnZSI6eyJtaW4iOjIxfX0sInVzZXJfaWQiOiIxMDAwMDM2MzQwOTQxMzkifQ");
         getWicketTester().getRequest().getPostParameters().setParameterValue("email", "fakeEmail"); // Prevent FB api call
         getWicketTester().startPage(FacebookAppStart.class);
-        getWicketTester().assertNoErrorMessage();
-        assertStringNotInHeader(FACEBOOK_OAUTH_REDIRECT);
-        getWicketTester().assertRenderedPage(FacebookAppNickName.class);
-        // Enter valid nickname
-        FormTester formTester = getWicketTester().newFormTester("form");
-        formTester.setValue("name", "xx2");
-        formTester.submit("goButton");
         getWicketTester().assertRenderedPage(Game.class);
-        Page gamePage = getWicketTester().getLastRenderedPage();
-        Assert.assertEquals(TEST_LEVEL_TASK_1_1_SIMULATED_ID, gamePage.getPageParameters().get("taskId").toInt());
-        User user = getUser();
-        Assert.assertEquals(User.SocialNet.FACEBOOK, user.getSocialNet());
-        Assert.assertEquals("xx2", user.getUsername());
-        Assert.assertEquals("fakeEmail", user.getEmail());
-        Assert.assertEquals("100003634094139", user.getSocialNetUserId());
+        Assert.assertNull(getUser());
         endHttpRequestAndOpenSessionInViewFilter();
         endHttpSession();
-        */
     }
 
     @Test
@@ -152,7 +104,6 @@ public class TestWicketFacebookApp extends AbstractServiceTest {
         getWicketTester().getRequest().getPostParameters().setParameterValue("signed_request", "3RaYyXwkwhCc4OlVfDhAU9Y-O_pyqN4mgE7JyzPwcIc.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImlzc3VlZF9hdCI6MTM0MzE0NjYzOSwidXNlciI6eyJjb3VudHJ5IjoiY2giLCJsb2NhbGUiOiJlbl9VUyIsImFnZSI6eyJtaW4iOjIxfX19");
         getWicketTester().startPage(FacebookAppStart.class);
         getWicketTester().assertNoErrorMessage();
-        assertStringInHeader(FACEBOOK_OAUTH_REDIRECT);
         endHttpRequestAndOpenSessionInViewFilter();
         // User denies access
         beginHttpRequestAndOpenSessionInViewFilter();
@@ -180,7 +131,6 @@ public class TestWicketFacebookApp extends AbstractServiceTest {
         // First authorization but already logged in
         getWicketTester().getRequest().getPostParameters().setParameterValue("signed_request", "3RaYyXwkwhCc4OlVfDhAU9Y-O_pyqN4mgE7JyzPwcIc.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImlzc3VlZF9hdCI6MTM0MzE0NjYzOSwidXNlciI6eyJjb3VudHJ5IjoiY2giLCJsb2NhbGUiOiJlbl9VUyIsImFnZSI6eyJtaW4iOjIxfX19");
         getWicketTester().startPage(FacebookAppStart.class);
-        assertStringNotInHeader(FACEBOOK_OAUTH_REDIRECT);
         getWicketTester().assertErrorMessages("You are already logged in as U1. Please logout before you login via Facebook");
         endHttpRequestAndOpenSessionInViewFilter();
         endHttpSession();
@@ -201,7 +151,6 @@ public class TestWicketFacebookApp extends AbstractServiceTest {
         // First authorization but already logged in
         getWicketTester().getRequest().getPostParameters().setParameterValue("signed_request", "v3-O8s1WrS9B2XnYXpRo61n2hKc9wboofRDHOxcF8XI.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImV4cGlyZXMiOjEzNDMxNTI4MDAsImlzc3VlZF9hdCI6MTM0MzE0NjY4Mywib2F1dGhfdG9rZW4iOiJBQUFFa3RlWVZ1WkNNQkFDS29mOGpkWDMxcnVTWkN3RXFuRnFWd3Z2NnBBNldNMTVaQ1V6bzlRNmliUXJiWGtRVkJOeEF0UDJmc2EzVzY3ZXJITW5EWkFvNlZHRzVPajg4U2FJMWZOYkVyYjhCeDBuOURRWkIyIiwidXNlciI6eyJjb3VudHJ5IjoiY2giLCJsb2NhbGUiOiJlbl9VUyIsImFnZSI6eyJtaW4iOjIxfX0sInVzZXJfaWQiOiIxMDAwMDM2MzQwOTQxMzkifQ");
         getWicketTester().startPage(FacebookAppStart.class);
-        assertStringNotInHeader(FACEBOOK_OAUTH_REDIRECT);
         getWicketTester().assertErrorMessages("You are already logged in as U1. Please logout before you login via Facebook");
         endHttpRequestAndOpenSessionInViewFilter();
         endHttpSession();
@@ -228,7 +177,6 @@ public class TestWicketFacebookApp extends AbstractServiceTest {
         // First authorization but already logged in
         getWicketTester().getRequest().getPostParameters().setParameterValue("signed_request", "v3-O8s1WrS9B2XnYXpRo61n2hKc9wboofRDHOxcF8XI.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImV4cGlyZXMiOjEzNDMxNTI4MDAsImlzc3VlZF9hdCI6MTM0MzE0NjY4Mywib2F1dGhfdG9rZW4iOiJBQUFFa3RlWVZ1WkNNQkFDS29mOGpkWDMxcnVTWkN3RXFuRnFWd3Z2NnBBNldNMTVaQ1V6bzlRNmliUXJiWGtRVkJOeEF0UDJmc2EzVzY3ZXJITW5EWkFvNlZHRzVPajg4U2FJMWZOYkVyYjhCeDBuOURRWkIyIiwidXNlciI6eyJjb3VudHJ5IjoiY2giLCJsb2NhbGUiOiJlbl9VUyIsImFnZSI6eyJtaW4iOjIxfX0sInVzZXJfaWQiOiIxMDAwMDM2MzQwOTQxMzkifQ");
         getWicketTester().startPage(FacebookAppStart.class);
-        assertStringNotInHeader(FACEBOOK_OAUTH_REDIRECT);
         Assert.assertEquals("xxx", getUser().getUsername());
         endHttpRequestAndOpenSessionInViewFilter();
         endHttpSession();
@@ -244,7 +192,7 @@ public class TestWicketFacebookApp extends AbstractServiceTest {
 
         beginHttpSession();
         beginHttpRequestAndOpenSessionInViewFilter();
-        // First authorization -> OAuth dialog
+        // First authorization
         getWicketTester().getRequest().getPostParameters().setParameterValue("signed_request", "3RaYyXwkwhCc4OlVfDhAU9Y-O_pyqN4mgE7JyzPwcIc.eyJhbGdvcml0aG0iOiJITUFDLVNIQTI1NiIsImlzc3VlZF9hdCI6MTM0MzE0NjYzOSwidXNlciI6eyJjb3VudHJ5IjoiY2giLCJsb2NhbGUiOiJlbl9VUyIsImFnZSI6eyJtaW4iOjIxfX19");
         getWicketTester().startPage(FacebookAppStart.class);
         endHttpRequestAndOpenSessionInViewFilter();
@@ -253,7 +201,7 @@ public class TestWicketFacebookApp extends AbstractServiceTest {
         beginHttpSession();
         beginHttpRequestAndOpenSessionInViewFilter();
         List<DbPageAccess> dbPageAccesses = loadAll(DbPageAccess.class);
-        Assert.assertEquals(1, dbPageAccesses.size());
+        Assert.assertEquals(2, dbPageAccesses.size());
         Assert.assertEquals(FacebookAppStart.class.getName(), dbPageAccesses.get(0).getPage());
         Assert.assertEquals("---User NOT Authorized by Facebook--- Query Parameters: ", dbPageAccesses.get(0).getAdditional());
         endHttpRequestAndOpenSessionInViewFilter();
