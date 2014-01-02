44f602d8aae257b9cc301aaf792f381c0bf6ddc6
beat.keller@btxtech.com <beat.keller@btxtech.com@4948f9da-d142-11de-aacf-132551bdafba>
2014-01-02 11:24:32 +0000

    Unload load tutorial task more tolerant

--- trunk/controltheland/src/main/java/com/btxtech/game/wicket/pages/mgmt/tutorial/ContainedInListenerTaskEditor.java
+++ trunk/controltheland/src/main/java/com/btxtech/game/wicket/pages/mgmt/tutorial/ContainedInListenerTaskEditor.java
@@ -14,15 +14,14 @@
 package com.btxtech.game.wicket.pages.mgmt.tutorial;
 
 import com.btxtech.game.jsre.common.gameengine.syncObjects.SyncItemListener;
-import com.btxtech.game.jsre.common.tutorial.SyncItemListenerTaskConfig;
 import com.btxtech.game.services.common.RuServiceHelper;
-import com.btxtech.game.services.tutorial.DbSyncItemListenerTaskConfig;
+import com.btxtech.game.services.tutorial.DbContainedInListenerTaskConfig;
 import com.btxtech.game.wicket.pages.mgmt.MgmtWebPage;
 import com.btxtech.game.wicket.uiservices.BaseItemTypePanel;
 import com.btxtech.game.wicket.uiservices.RuModel;
 import com.btxtech.game.wicket.uiservices.TerrainLinkHelper;
 import org.apache.wicket.markup.html.form.Button;
-import org.apache.wicket.markup.html.form.ChoiceRenderer;
+import org.apache.wicket.markup.html.form.CheckBox;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
@@ -36,16 +35,16 @@ import java.util.Arrays;
  * Date: 22.12.2013
  * Time: 23:29:54
  */
-public class SyncItemListenerTaskEditor extends MgmtWebPage {
+public class ContainedInListenerTaskEditor extends MgmtWebPage {
     @SpringBean
-    private RuServiceHelper<DbSyncItemListenerTaskConfig> ruTaskServiceHelper;
+    private RuServiceHelper<DbContainedInListenerTaskConfig> ruTaskServiceHelper;
 
-    public SyncItemListenerTaskEditor(DbSyncItemListenerTaskConfig dbSyncItemListenerTaskConfig, final TerrainLinkHelper terrainLinkHelper) {
+    public ContainedInListenerTaskEditor(DbContainedInListenerTaskConfig dbContainedInListenerTaskConfig, final TerrainLinkHelper terrainLinkHelper) {
         add(new FeedbackPanel("msgs"));
 
-        final Form<DbSyncItemListenerTaskConfig> form = new Form<>("taskForm", new CompoundPropertyModel<>(new RuModel<DbSyncItemListenerTaskConfig>(dbSyncItemListenerTaskConfig, DbSyncItemListenerTaskConfig.class) {
+        final Form<DbContainedInListenerTaskConfig> form = new Form<>("taskForm", new CompoundPropertyModel<>(new RuModel<DbContainedInListenerTaskConfig>(dbContainedInListenerTaskConfig, DbContainedInListenerTaskConfig.class) {
             @Override
-            protected RuServiceHelper<DbSyncItemListenerTaskConfig> getRuServiceHelper() {
+            protected RuServiceHelper<DbContainedInListenerTaskConfig> getRuServiceHelper() {
                 return ruTaskServiceHelper;
             }
         }));
@@ -53,13 +52,14 @@ public class SyncItemListenerTaskEditor extends MgmtWebPage {
         form.add(new CommonTaskEditorPanel("commonTaskEditorPanel", terrainLinkHelper));
 
         form.add(new BaseItemTypePanel("syncItemTypeToWatch"));
-        form.add(new DropDownChoice<>("syncItemChange", Arrays.asList(SyncItemListener.Change.values())));
+        form.add(new CheckBox("containedIn"));
+        form.add(new CheckBox("checkContainedInOnStart"));
 
         form.add(new Button("save") {
 
             @Override
             public void onSubmit() {
-                ruTaskServiceHelper.updateDbEntity((DbSyncItemListenerTaskConfig) form.getDefaultModelObject());
+                ruTaskServiceHelper.updateDbEntity((DbContainedInListenerTaskConfig) form.getDefaultModelObject());
             }
         });
         form.add(new Button("back") {
