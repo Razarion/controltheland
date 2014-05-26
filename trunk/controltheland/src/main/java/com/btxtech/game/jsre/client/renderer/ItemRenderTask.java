f68913dad3d70ba8dbac6d34cb4130a8992e1147
beat.keller@btxtech.com <beat.keller@btxtech.com@4948f9da-d142-11de-aacf-132551bdafba>
2014-05-26 14:02:05 +0000

    Render resource field below other SynItems

--- trunk/controltheland/src/main/java/com/btxtech/game/jsre/client/renderer/ItemRenderTask.java
+++ trunk/controltheland/src/main/java/com/btxtech/game/jsre/client/renderer/ItemRenderTask.java
@@ -6,9 +6,11 @@ import com.btxtech.game.jsre.client.common.Rectangle;
 import com.btxtech.game.jsre.common.gameengine.itemType.ItemTypeSpriteMap;
 import com.btxtech.game.jsre.common.gameengine.syncObjects.SyncItem;
 import com.btxtech.game.jsre.common.gameengine.syncObjects.SyncItemArea;
+import com.btxtech.game.jsre.common.gameengine.syncObjects.SyncResourceItem;
 import com.google.gwt.canvas.dom.client.Context2d;
 import com.google.gwt.dom.client.ImageElement;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -26,6 +28,7 @@ public class ItemRenderTask extends AbstractRenderTask {
     @Override
     public void render(long timeStamp, Collection<SyncItem> itemsInView, Rectangle viewRect, Rectangle tileViewRect) {
         context2d.setFillStyle("#999999");
+        Collection<SyncItem> noneResourceItems = new ArrayList<>(itemsInView.size());
         for (SyncItem syncItem : itemsInView) {
             ImageElement imageElement = ItemTypeImageLoaderContainer.getInstance().getImage(syncItem.getItemType());
             if (imageElement == null) {
@@ -41,23 +44,37 @@ public class ItemRenderTask extends AbstractRenderTask {
                 context2d.fill();
                 context2d.setGlobalAlpha(1.0);
             }
-            ItemTypeSpriteMap itemTypeSpriteMap = syncItem.getItemType().getItemTypeSpriteMap();
-            Index absoluteImagePosition = syncItemArea.getTopLeftFromImagePosition();
-            Index relativeImagePosition = new Index(absoluteImagePosition.getX() - viewRect.getX(), absoluteImagePosition.getY() - viewRect.getY());
-
-            Index offset = itemTypeSpriteMap.getItemTypeImageOffset(syncItem, timeStamp);
-            context2d.drawImage(imageElement,
-                    offset.getX(), // the x coordinate of the upper-left corner of the source rectangle
-                    offset.getY(), // the y coordinate of the upper-left corner of the source rectangle
-                    itemTypeSpriteMap.getImageWidth(),// the width of the source rectangle
-                    itemTypeSpriteMap.getImageHeight(),// sh the width of the source rectangle
-                    relativeImagePosition.getX(),// the x coordinate of the upper-left corner of the destination rectangle
-                    relativeImagePosition.getY(),// the y coordinate of the upper-left corner of the destination rectangle
-                    itemTypeSpriteMap.getImageWidth(),// the width of the destination rectangle
-                    itemTypeSpriteMap.getImageHeight()// the height of the destination rectangle
-            );
+            // Render the resources below the items
+            if(syncItem instanceof SyncResourceItem) {
+                renderSyncItem(timeStamp, viewRect, syncItem, imageElement, syncItemArea);
+            }else{
+                noneResourceItems.add(syncItem);
+            }
+        }
+        for (SyncItem syncItem : noneResourceItems) {
+            SyncItemArea syncItemArea = syncItem.getSyncItemArea();
+            ImageElement imageElement = ItemTypeImageLoaderContainer.getInstance().getImage(syncItem.getItemType());
+            renderSyncItem(timeStamp, viewRect, syncItem, imageElement, syncItemArea);
         }
 
         ItemTypeImageLoaderContainer.getInstance().startLoad();
+    }
+
+    private void renderSyncItem(long timeStamp, Rectangle viewRect, SyncItem syncItem, ImageElement imageElement, SyncItemArea syncItemArea) {
+        ItemTypeSpriteMap itemTypeSpriteMap = syncItem.getItemType().getItemTypeSpriteMap();
+        Index absoluteImagePosition = syncItemArea.getTopLeftFromImagePosition();
+        Index relativeImagePosition = new Index(absoluteImagePosition.getX() - viewRect.getX(), absoluteImagePosition.getY() - viewRect.getY());
+
+        Index offset = itemTypeSpriteMap.getItemTypeImageOffset(syncItem, timeStamp);
+        context2d.drawImage(imageElement,
+                offset.getX(), // the x coordinate of the upper-left corner of the source rectangle
+                offset.getY(), // the y coordinate of the upper-left corner of the source rectangle
+                itemTypeSpriteMap.getImageWidth(),// the width of the source rectangle
+                itemTypeSpriteMap.getImageHeight(),// sh the width of the source rectangle
+                relativeImagePosition.getX(),// the x coordinate of the upper-left corner of the destination rectangle
+                relativeImagePosition.getY(),// the y coordinate of the upper-left corner of the destination rectangle
+                itemTypeSpriteMap.getImageWidth(),// the width of the destination rectangle
+                itemTypeSpriteMap.getImageHeight()// the height of the destination rectangle
+        );
     }
 }
